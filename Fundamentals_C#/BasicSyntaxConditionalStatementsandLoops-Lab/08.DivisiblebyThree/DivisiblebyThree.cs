52f9556522c9dc3e231a8db55c37f7739a4f2aac
Glavyanov <dimitarglavianov@gmail.com>
2021-08-29 10:41:24 +0300

    Contains solutions for labs, exercises and exams

---
+++ Fundamentals_C#/BasicSyntaxConditionalStatementsandLoops-Lab/08.DivisiblebyThree/DivisiblebyThree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _08DivisiblebyThree
+{
+    class DivisiblebyThree
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i <= 100; i +=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Fundamentals_C#/BasicSyntaxConditionalStatementsandLoops-Lab/12.EvenNumber/EvenNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _12.EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            while ((n = int.Parse(Console.ReadLine())) % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+            }
+            Console.WriteLine("The number is: {0}", n = Math.Abs(n));
+        }
+    }
+}
