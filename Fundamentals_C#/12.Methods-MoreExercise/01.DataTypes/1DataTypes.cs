ac267363b0b48ac494ccc52a77cea9a0e64032c0
Glavyanov <dimitarglavianov@gmail.com>
2021-09-11 21:33:19 +0300

    Contains solutions for Method-MoreExercise

---
+++ Fundamentals_C#/12.Methods-MoreExercise/01.DataTypes/1DataTypes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01DataTypes
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            string typeData = Console.ReadLine();
+            string input = Console.ReadLine();
+            GetAllOperations(typeData, input);
+        }
+
+        private static void GetAllOperations(string type, string value)
+        {
+            switch (type)
+            {
+                case "int":
+                    int inputNum = int.Parse(value);
+                    inputNum *= 2;
+                    Console.WriteLine(inputNum);
+                    break;
+                case "real":
+                    double inputReal = double.Parse(value);
+                    inputReal *= 1.5;
+                    Console.WriteLine($"{inputReal:f2}");
+                    break;
+                case "string":
+                    Console.WriteLine("${0}$", value);
+                    break;
+                default:
+                    break;
+            }
+        }
+       
+    }
+}
---
+++ Fundamentals_C#/12.Methods-MoreExercise/02.CenterPoint/2CenterPoint.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MethodsMoreExersice1
+{
+    class CenterPoint
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            int result = GetPointCloseToZero(GetFirstPoint(x1, y1), GetSecondPoint(x2, y2));
+            if (result < 0)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else if (result > 0)
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+        }
+
+        private static int GetPointCloseToZero(double result1, double result2)
+        {
+            if (result1 < result2)
+            {
+                return -1;
+            }
+            else if (result2 < result1)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        private static double GetFirstPoint(double x1, double y1)
+        {
+            double result = Math.Sqrt(Math.Abs(x1 * x1) + Math.Abs(y1 * y1));
+            return result;
+        }
+
+        private static double GetSecondPoint(double x2, double y2)
+        {
+            double result = Math.Sqrt(Math.Abs(x2 * x2) + Math.Abs(y2 * y2));
+            return result;
+        }
+
+
+    }
+}
