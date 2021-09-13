033f64f0830918085250034e432250f7ce96e05a
Glavyanov <dimitarglavianov@gmail.com>
2021-09-13 23:12:05 +0300

    Contains Solutions for Methods-Lab

---
+++ Fundamentals_C#/13.Methods-Lab/01.SignofIntegerNumbers/1SignofIntegerNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01SignofIntegerNumbers
+{
+    class SignofIntegerNumbers
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            SignOfNumbers(num);
+        }
+
+        private static void SignOfNumbers(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine("The number {0} is positive.", number);
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number {0} is negative.", number);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is zero.", number);
+            }
+        }
+    }
+}
---
+++ Fundamentals_C#/13.Methods-Lab/09.GreaterOfTwoValues/9GreaterOfTwoValues.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace _09GreaterOfTwoValues
+{
+    class GreaterOfTwoValues
+    {
+        static void Main(string[] args)
+        {
+            string typeValue = Console.ReadLine();
+            string valueOne = Console.ReadLine();
+            string valueTwo = Console.ReadLine();
+            switch (typeValue)
+            {
+                case "int":
+                    int convertValueOne = int.Parse(valueOne);
+                    int convertValueTwo = int.Parse(valueTwo);
+                    int result = GetMax(convertValueOne, convertValueTwo);
+                    Console.WriteLine(result);
+                    break;
+                case "char":
+                    char convertchrValueOne = char.Parse(valueOne);
+                    char convertchrValueTwo = char.Parse(valueTwo);
+                    char resultchr = GetMax(convertchrValueOne, convertchrValueTwo);
+                    Console.WriteLine(resultchr);
+                    break;
+                case "string":
+                    string resultstr = GetMax(valueOne, valueTwo);
+                    Console.WriteLine(resultstr);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private static int GetMax(int valueOne, int valueTwo)
+        {
+            int result = 0;
+            if (valueOne > valueTwo)
+            {
+                result = valueOne;
+            }
+            else if (valueOne < valueTwo)
+            {
+                result = valueTwo;
+            }  
+            else
+            {
+                result = valueTwo;
+            }
+            return result;
+        }
+        private static char GetMax(char valueOne, char valueTwo)
+        {
+            char result = '\0' ;
+            if (valueOne.CompareTo(valueTwo) > 0)
+            {
+                result = valueOne;
+            }
+            else
+            {
+                result = valueTwo;
+            }
+            return result;
+        }
+        private static string GetMax(string valueOne, string valueTwo)
+        {
+            string result = string.Empty;
+            if (valueOne.CompareTo(valueTwo) > 0)
+            {
+                result = valueOne;
+            }
+            else
+            {
+                result = valueTwo;
+            }
+            return result;
+
+        }
+    }
+}
