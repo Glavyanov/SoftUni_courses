c2597800c27d0c766aee4c8426162e82eac41e1b
Glavyanov <dimitarglavianov@gmail.com>
2021-09-22 20:23:37 +0300

    Contains Solutions for problems in Associative Arrays - Exercise

---
+++ Fundamentals_C#/24.Associative Arrays - Exercise/05.SoftUniParking/5SoftUniParking.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05.SoftUniParking
+{
+    class SoftUniParking
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var logger = new Dictionary<string, string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string action = input[0];
+                if (action == "register")
+                {
+                    string user = input[1];
+                    string plate = input[2];
+                    if (!logger.ContainsKey(user))
+                    {
+                        logger.Add(user, plate);
+                        Console.WriteLine($"{user} registered {plate} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {logger[user]}");
+                    }
+
+                }
+                else if (action == "unregister")
+                {
+                    string user = input[1];
+
+                    if (!logger.ContainsKey(user))
+                    {
+                        Console.WriteLine($"ERROR: user {user} not found");
+                    }
+                    else
+                    {
+                        logger.Remove(user);
+                        Console.WriteLine($"{user} unregistered successfully");
+                    }
+                }
+
+            }
+            foreach (var item in logger)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+    }
+}
---
+++ Fundamentals_C#/24.Associative Arrays - Exercise/09.ForceBook/9ForceBook.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _09.ForceBook
+{
+    class ForceBook
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, List<string>> sideAndUsers = new Dictionary<string, List<string>>();
+           
+            while (command != "Lumpawaroo")
+            {
+                if (command.Contains(" | "))
+                {
+                    string[] cmdArgs = command.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string side = cmdArgs[0];
+                    string user = cmdArgs[1];
+                    if (!sideAndUsers.ContainsKey(side))
+                    {
+                        if (!sideAndUsers.Any(x => x.Value.Contains(user)))
+                        {
+                            sideAndUsers[side] = new List<string>();
+                            sideAndUsers[side].Add(user);
+                        }
+                       
+                    }
+                    else
+                    {
+                        if (!sideAndUsers.Any(x => x.Value.Contains(user)))
+                        {
+                            sideAndUsers[side].Add(user);
+                        }
+                    }
+
+                }
+                else if (command.Contains(" -> "))
+                {
+                    string[] cmdArgs = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    string user = cmdArgs[0];
+                    string side = cmdArgs[1];
+                    if (!sideAndUsers.ContainsKey(side))
+                    {
+                        if (!sideAndUsers.Any(x => x.Value.Contains(user)))
+                        {
+                            sideAndUsers[side] = new List<string>();
+                            sideAndUsers[side].Add(user);
+                            JoinsTheSide(user, side);
+
+                        }
+                        else
+                        {
+                            if (string.Join("", sideAndUsers.Where(y => y.Value.Contains(user)).Select(x => x.Key)) != side)
+                            {
+                                if (sideAndUsers.Values.Any(x => x.Contains(user)))
+                                {
+                                    string sideToSubtract = string
+                                                             .Join("", sideAndUsers.Where(x => x.Value.Contains(user))
+                                                             .Select(x => x.Key));
+                                    sideAndUsers[sideToSubtract].Remove(user);
+                                }
+
+                            }
+                            sideAndUsers[side] = new List<string>();
+                            sideAndUsers[side].Add(user);
+                            JoinsTheSide(user, side);
+
+                        }
+
+                    }
+                    else
+                    {
+                        if (!sideAndUsers[side].Any(x => x == user))
+                        {
+                            if (string.Join("", sideAndUsers.Where(y => y.Value.Contains(user)).Select(x => x.Key)) != side)
+                            {
+                                if (sideAndUsers.Values.Any(x => x.Contains(user)))
+                                {
+                                    string sideToSubtract = string
+                                                             .Join("", sideAndUsers.Where(x => x.Value.Contains(user))
+                                                             .Select(x => x.Key));
+                                    sideAndUsers[sideToSubtract].Remove(user);
+                                }
+
+                            }
+                            sideAndUsers[side].Add(user);
+                            JoinsTheSide(user, side);
+                        }
+
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+            foreach (var (side, user) in sideAndUsers.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                if (user.Count > 0)
+                {
+                    Console.WriteLine($"Side: {side}, Members: {user.Count}");
+
+                }
+
+                foreach (var item in user.OrderBy(x => x))
+                {
+                    Console.WriteLine($"! {item}");
+                }
+
+            }
+        }
+
+        public static void JoinsTheSide(string user, string side)
+        {
+            Console.WriteLine($"{user} joins the {side} side!");
+        }
+    }
+}
