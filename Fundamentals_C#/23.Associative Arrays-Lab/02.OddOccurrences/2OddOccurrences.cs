e9fc6ef0c4877f4ca85829e89f14dad6ecb1f28a
Glavyanov <dimitarglavianov@gmail.com>
2021-09-22 20:04:13 +0300

    Contains Solutions for problems in Associative Arrays - Lab

---
+++ Fundamentals_C#/23.Associative Arrays-Lab/02.OddOccurrences/2OddOccurrences.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02.OddOccurrences
+{
+    class OddOccurrences
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> occurence = new Dictionary<string, int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (occurence.ContainsKey(words[i].ToLower()))
+                {
+                    occurence[words[i].ToLower()]++;
+                }
+                else
+                {
+                    occurence.Add(words[i].ToLower(), 1);
+                }
+
+            }
+            Console.WriteLine(string.Join(' ', occurence.Where(x => x.Value % 2 != 0).Select(x => x.Key)));
+
+        }
+    }
+}
