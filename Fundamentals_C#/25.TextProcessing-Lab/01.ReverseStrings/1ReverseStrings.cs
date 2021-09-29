29e1afb769dd6607eaa728e422d61f6d41e56132
Glavyanov <dimitarglavianov@gmail.com>
2021-09-29 23:25:23 +0300

    Contains Solutions for TextProcessing - Lab

---
+++ Fundamentals_C#/25.TextProcessing-Lab/01.ReverseStrings/1ReverseStrings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ReverseStrings
+{
+    class ReverseStrings
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                Console.WriteLine($"{command} = {string.Join("",command.Reverse())}");
+            }
+        }
+    }
+}
