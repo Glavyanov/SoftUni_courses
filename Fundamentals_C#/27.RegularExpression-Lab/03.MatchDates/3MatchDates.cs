def1cccc8a3f597335dd5de98ce1de2a4402b634
Glavyanov <dimitarglavianov@gmail.com>
2021-10-01 19:19:21 +0300

    Contains Solutions for Regular Expression - Lab

---
+++ Fundamentals_C#/27.RegularExpression-Lab/03.MatchDates/3MatchDates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    class MatchDates
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            MatchCollection validDate = Regex.Matches(input, @"\b(?<day>[0-9]{2})(?<separator>[-\/.])(?<month>[A-Z][a-z]{2})(\k<separator>)(?<year>[0-9]{4})\b");
+            
+            foreach (Match item in validDate)
+            {
+                var day = item.Groups["day"].Value;
+                var month = item.Groups["month"].Value;
+                var year = item.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+
+            }
+
+        }
+    }
+}
