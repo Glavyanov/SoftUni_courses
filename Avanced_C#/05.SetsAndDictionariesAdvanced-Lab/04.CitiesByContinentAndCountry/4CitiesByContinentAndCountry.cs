0c0f42be7495c97781f463b853f4bdc634b34366
Glavyanov <dimitarglavianov@gmail.com>
2021-10-09 19:15:08 +0300

    Contains solutions for problems in SetsAndDictionariesAdvanced - Lab

---
+++ Avanced_C#/05.SetsAndDictionariesAdvanced-Lab/04.CitiesByContinentAndCountry/4CitiesByContinentAndCountry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.CitiesByContinentAndCountry
+{
+    class CitiesByContinentAndCountry
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var locations = new Dictionary<string, Dictionary<string, List<string>>>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] assign = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (locations.ContainsKey(assign[0]))
+                {
+                    if (locations[assign[0]].ContainsKey(assign[1]))
+                    {
+                        locations[assign[0]][assign[1]].Add(assign[2]);
+                    }
+                    else
+                    {
+                        locations[assign[0]].Add(assign[1], new List<string>() { assign[2] });
+                    }
+                }
+                else
+                {
+                    locations.Add(assign[0], new Dictionary<string, List<string>>() { [assign[1]] = new List<string>() { assign[2] } });
+                }
+
+            }
+            foreach (var location in locations)
+            {
+                Console.WriteLine($"{location.Key}:");
+                foreach (var item in location.Value)
+                {
+                    Console.WriteLine($"  {item.Key} -> {string.Join(", ", item.Value)}");
+                }
+            }
+        }
+    }
+}
---
+++ Avanced_C#/05.SetsAndDictionariesAdvanced-Lab/05.RecordUniqueNames/5RecordUniqueNames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.RecordUniqueNames
+{
+    class RecordUniqueNames
+    {
+        static void Main(string[] args)
+        {
+
+            var set = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                set.Add(Console.ReadLine());
+            }
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
