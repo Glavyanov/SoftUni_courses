d5d5b1ac52de24d7e465061483a23b5c2e3fdf4b
Glavyanov <dimitarglavianov@gmail.com>
2021-10-09 19:15:38 +0300

    Contains solutions for problems in SetsAndDictionariesAdvanced - Lab

---
+++ Avanced_C#/05.SetsAndDictionariesAdvanced-Lab/06.ParkingLot/6ParkingLot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _06.ParkingLot
+{
+    class ParkingLot
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = Regex.Split(command, ", ");
+                if (cmdArgs[0].ToUpper() == "IN")
+                {
+                    cars.Add(cmdArgs[1]);
+                }
+                else if (cmdArgs[0].ToUpper() == "OUT")
+                {
+                    if (cars.Contains(cmdArgs[1]))
+                    {
+                        cars.Remove(cmdArgs[1]);
+                    }
+                }
+
+            }
+            if (cars.Count > 0)
+            {
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ Avanced_C#/05.SetsAndDictionariesAdvanced-Lab/07.SoftUniParty/7SoftUniParty.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _07.SoftUniParty
+{
+    class SoftUniParty
+    {
+        static void Main(string[] args)
+        {
+            string guest = Console.ReadLine();
+            HashSet<string> loggerVip = new HashSet<string>();
+            HashSet<string> loggerRegular = new HashSet<string>();
+            Regex regexVip = new Regex(@"^[0-9]{1}.{7}$");
+            Regex regexRegular = new Regex(@"^.{8}$");
+            while (guest != "PARTY")
+            {
+                Match matchVip = regexVip.Match(guest);
+                Match match = regexRegular.Match(guest);
+                if (matchVip.Success)
+                {
+                    loggerVip.Add(guest);
+                }
+                else if (match.Success)
+                {
+                    loggerRegular.Add(guest);
+                }
+                guest = Console.ReadLine();
+            }
+            string arrival = Console.ReadLine();
+            while (arrival != "END")
+            {
+                if (loggerVip.Contains(arrival))
+                {
+                    loggerVip.Remove(arrival);
+                }
+                else if (loggerRegular.Contains(arrival))
+                {
+                    loggerRegular.Remove(arrival);
+                }
+                arrival = Console.ReadLine();
+            }
+            Console.WriteLine(loggerVip.Count + loggerRegular.Count);
+            if (loggerVip.Count > 0)
+            {
+                foreach (var item in loggerVip)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            if (loggerRegular.Count > 0)
+            {
+                foreach (var item in loggerRegular)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
