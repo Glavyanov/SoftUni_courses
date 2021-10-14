669528e0dd8383a4b57f6df30e20df60941340c5
Glavyanov <dimitarglavianov@gmail.com>
2021-10-14 12:47:49 +0300

    Contains solutions for problems in Generics - Lab

---
+++ Avanced_C#/11.Generics-Lab/02.ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T item)
+        {
+            var arr = new T[lenght];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = item;
+            }
+            return arr;
+        }
+    }
+}
