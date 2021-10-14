6146e5a37cd1097c65696c7ba267636b07b6b6de
Glavyanov <dimitarglavianov@gmail.com>
2021-10-14 12:48:19 +0300

    Contains solutions for problems in Generics - Lab

---
+++ Avanced_C#/11.Generics-Lab/03.GenericScale/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var intCheck = new EqualityScale<int>(5, 8);
+            var intCheckNext = new EqualityScale<int>(6, 6);
+
+            Console.WriteLine(intCheck.AreEqual());
+            Console.WriteLine(intCheckNext.AreEqual());
+        }
+    }
+}
