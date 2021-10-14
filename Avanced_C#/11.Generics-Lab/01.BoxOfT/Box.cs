ded39671736931eec36cf71b4bb63a17631f40eb
Glavyanov <dimitarglavianov@gmail.com>
2021-10-14 12:46:47 +0300

    Contains solutions for problems in Generics - Lab

---
+++ Avanced_C#/11.Generics-Lab/01.BoxOfT/Box.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public int Count
+        {
+            get
+            {
+                return data.Count; // с това изпозваме Count на List-a data, вместо наш собствен който постоянно да променяме в методите.
+            }
+        }
+
+        public Box()
+        {
+            data = new List<T>();
+            
+        }
+
+        public void Add(T element)
+        {
+            data.Add(element);
+            
+        }
+
+        public T Remove()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Can not remove from empty collection");
+            }
+
+            T result = data.Last();
+            data.RemoveAt(data.Count-1);
+
+            return result;
+        }
+    }
+}
