5ff25f534f7fd9a3daf365b540e62a7eaece1321
Glavyanov <dimitarglavianov@gmail.com>
2021-10-27 20:52:40 +0300

    Contains solutions for problems in Generics - Lab

---
+++ Avanced_C#/12.Generics-Exercise/02.GenericBoxOfInteger/2GenericBoxOfInteger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class GenericBoxOfInteger
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<int>> boxs = new List<Box<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(input);
+                boxs.Add(box);
+            }
+
+            foreach (var box in boxs)
+            {
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
---
+++ Avanced_C#/12.Generics-Exercise/02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public T Value => this.value;
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
