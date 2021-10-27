974f602fb965b8cf577c3a8e374ce6b53f4a1841
Glavyanov <dimitarglavianov@gmail.com>
2021-10-27 20:51:52 +0300

    Contains solutions for problems in Generics - Exercise

---
+++ Avanced_C#/12.Generics-Exercise/01.GenericBoxOfString/1GenericBoxOfString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.GenericBoxOfString
+{
+    class GenericBoxOfString
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> boxs = new List<Box<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> single = new Box<string>(input);
+                boxs.Add(single);
+            }
+
+            foreach (var box in boxs)
+            {
+                Console.WriteLine(box);
+            }
+        }
+
+    }
+}
