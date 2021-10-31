b3c243f4d8dacef35d6b7915f2b475abfed4e35e
Glavyanov <dimitarglavianov@gmail.com>
2021-10-31 13:05:42 +0200

    Contains Solutions for Iterators and Comparators - Lab

---
+++ Avanced_C#/13.IteratorsAndComparators-Lab/02.LibraryIterator/Library.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public  class Library : IEnumerable<Book>
+    {
+        
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        private readonly List<Book> books;
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+
+            private int currenIndex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = books.ToList();
+            }
+
+            public Book Current => this.books[this.currenIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+                
+            }
+
+            public bool MoveNext()
+            {
+                return ++this.currenIndex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.currenIndex = -1;
+            }
+        }
+    }
+}
---
+++ Avanced_C#/13.IteratorsAndComparators-Lab/02.LibraryIterator/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Winnie the Pooh", 1967, "A. A. Milne", "Andersen");
+            Book bookTwo = new Book("Vinnie the Pooh", 1968, "A. A. Milne", "Andersen");
+            Library library = new Library();
+            library = new Library(bookOne, bookTwo);
+            Library library1 = new Library();
+            foreach (var item in library)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+    }
+}
