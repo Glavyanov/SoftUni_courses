cb0e9620086e23fc48faeef67ffadb887ee48dc7
Glavyanov <dimitarglavianov@gmail.com>
2021-10-31 13:12:07 +0200

    Contains Solutions for Iterators and Comparators - Exercise

---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> items;
+
+        private int index;
+
+        public ListyIterator(params T[] items)
+        {
+            this.items = items.ToList();
+            this.index = 0;
+        }
+
+        public bool Move() => this.HasNext() ? this.index++ > -1 : this.HasNext();
+        //{
+        //    if (this.HasNext())
+        //    {
+        //        this.index++;
+        //        return true;
+        //    }
+        //    return false;
+        //}
+
+        public bool HasNext() => this.index < this.items.Count - 1;
+
+        public void Print()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(this.items[this.index]);
+        }
+        
+    }
+}
---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/01.ListyIterator/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                                      .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                      .Skip(1)
+                                      .ToArray();
+
+            ListyIterator<string> iterator = new ListyIterator<string>(input);
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Print")
+                {
+                    try
+                    {
+                        iterator.Print();
+
+                    }
+                    catch (Exception excptn)
+                    {
+                        Console.WriteLine(excptn.Message);
+                    }
+
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(iterator.HasNext());
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(iterator.Move());
+                }
+
+            }
+        }
+    }
+}
