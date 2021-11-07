8b4876261e3712a04359d59078e841073a01c6da
Glavyanov <dimitarglavianov@gmail.com>
2021-11-07 17:16:04 +0200

    Contains Solution for problem in Inheritance - Lab

---
+++ OOP_C#/01.Inheritance-Lab/02.MultipleInheritance/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
