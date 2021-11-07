72a8d479592724a89e77a552912e88825d24b23e
Glavyanov <dimitarglavianov@gmail.com>
2021-11-07 16:49:53 +0200

    Contains Solution for problem in Inheritance - Exercise

---
+++ OOP_C#/02.Inheritance-Exercise/01.Person/Child.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) 
+            : base(name, age)
+        {
+           
+        }
+        public override int Age 
+        {
+            get
+            {
+                return base.Age;
+            }
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("The age cannot be greather than 15.");
+                }
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ OOP_C#/02.Inheritance-Exercise/01.Person/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Person
+{
+    public class Person
+    {
+        private  int age;
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age) : this()
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("The age cannot be nagative.");
+                }
+                this.age = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+
+    }
+}
