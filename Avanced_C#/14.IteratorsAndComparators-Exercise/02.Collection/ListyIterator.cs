13425bf44bb3eb66f21f16e3a751dddb27136b0b
Glavyanov <dimitarglavianov@gmail.com>
2021-11-09 17:56:19 +0200

    Contains Solutions for Iterators and Comparators - Exercise

---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/02.Collection/ListyIterator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.Collection
+{
+    public class ListyIterator<T> :IEnumerable<T>
+    {
+        private List<T> items;
+
+        private int index;
+
+        public ListyIterator(params T[] items)
+        {
+            this.items = items.ToList();
+            this.index = 0;
+        }
+
+        public bool Move() => this.HasNext() ? this.index++ > -1 : this.HasNext();
+
+        public bool HasNext() => this.index < this.items.Count - 1;
+
+        public void Print()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(this.items[this.index]);
+        }
+
+        public void PrintAll()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.Write(string.Join(' ',this.items));
+            Console.WriteLine();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/02.Collection/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _02.Collection
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                                      .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                      .Skip(1)
+                                      .ToArray();
+
+            ListyIterator<string> iterator = new ListyIterator<string>(input);
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Print")
+                {
+                    try
+                    {
+                        iterator.Print();
+
+                    }
+                    catch (InvalidOperationException excptn)
+                    {
+                        Console.WriteLine(excptn.Message);
+                    }
+
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(iterator.HasNext());
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(iterator.Move());
+                }
+                else if (command == "PrintAll")
+                {
+                    try
+                    {
+                        iterator.PrintAll();
+
+                    }
+                    catch (InvalidOperationException excptn)
+                    {
+                        Console.WriteLine(excptn.Message);
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+
