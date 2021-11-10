3c8a94f71c7dbbe655aaf1b66a819b5268db4906
Glavyanov <dimitarglavianov@gmail.com>
2021-11-10 20:21:27 +0200

    Contains Solutions for Iterators and Comparators - Exercise

---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private readonly List<int> stones;
+
+        public Lake(params int[] stones)
+        {
+            this.stones = stones.ToList();
+            this.Count = this.stones.Count;
+        }
+
+        public int Count { get; private set; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stones.Count; i += 2)
+            {
+                yield return this.stones[i];
+            }
+            this.Count = this.Count % 2 == 0 ? this.Count : --this.Count;
+
+            for (int i = this.Count - 1; i >= 0; i -= 2)
+            {
+                yield return this.stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/04.Froggy/StarUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    class StarUp
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+            Lake lake = new Lake(input);
+            Console.WriteLine(string.Join(", ",lake));
+        }
+    }
+}
