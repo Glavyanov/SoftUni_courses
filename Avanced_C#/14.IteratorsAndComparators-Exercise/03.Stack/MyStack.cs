c87bc76e886feb0ba943721c202ed16fa600b824
Glavyanov <dimitarglavianov@gmail.com>
2021-11-10 20:20:11 +0200

    Contains Solutions for Iterators and Comparators - Exercise

---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/03.Stack/MyStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> items;
+
+        public int Count { get; private set; }
+
+        public MyStack()
+        {
+            this.items = new List<T>();
+            this.Count = 0;
+        }
+
+        public void Push(T item)
+        {
+            this.items.Add(item);
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            T item = this.items[this.items.Count - 1];
+            this.items.RemoveAt(this.items.Count - 1);
+            this.Count--;
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return this.items[i];
+            }
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/03.Stack/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _03.Stack
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stackNums = new MyStack<int>();
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command
+                                  .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                if (input[0] == "Push")
+                {
+                    for (int i = 1; i < input.Length; i++)
+                    {
+                        stackNums.Push(int.Parse(input[i]));
+                    }
+                }
+                else if (input[0] == "Pop")
+                {
+                    try
+                    {
+                        stackNums.Pop();
+                    }
+                    catch (InvalidOperationException excptn)
+                    {
+                        Console.WriteLine(excptn.Message);
+                    }
+
+                }
+            }
+            foreach (var item in stackNums)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, stackNums));
+        }
+    }
+}
