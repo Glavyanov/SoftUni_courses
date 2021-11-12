ea199df01573d5b140e092332f8260b63633def9
Glavyanov <dimitarglavianov@gmail.com>
2021-11-12 09:50:41 +0200

    Contains Solutions for Iterators and Comparators - Exercise

---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/05.ComparingObjects/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int year, string town) : this()
+        {
+            this.Name = name;
+            this.Year = year;
+            this.Town = town;
+        }
+        public string Name { get; private set; }
+
+        public int Year { get; private set; }
+
+        public string  Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            return this.Name.CompareTo(other.Name) != 0 ? this.Name.CompareTo(other.Name) : this.Year.CompareTo(other.Year) != 0 ? this.Year.CompareTo(other.Year) : this.Town.CompareTo(other.Town) != 0 ? this.Town.CompareTo(other.Town) : this.Town.CompareTo(other.Town); ;
+        }
+    }
+}
---
+++ Avanced_C#/14.IteratorsAndComparators-Exercise/05.ComparingObjects/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person("g", 2, "gg");
+            List<Person> persons = new List<Person>();
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(cmdArgs[0], int.Parse(cmdArgs[1]), cmdArgs[2]);
+                persons.Add(person);
+            }
+            int count = int.Parse(Console.ReadLine());
+            Person personToCompare = new Person();
+            if (count > 0 && count <= persons.Count)
+            {
+                personToCompare = persons[count - 1];
+
+            }
+            int countMathes = 0;
+            int countNonMathes = 0;
+            foreach (var item in persons)
+            {
+                int result = personToCompare.CompareTo(item);
+                if (result != 0)
+                {
+                    countNonMathes++;
+                }
+                else
+                {
+                    countMathes++;
+                }
+
+            }
+            if (countMathes == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{countMathes} {countNonMathes} {persons.Count}");
+            }
+        }
+    }
+}
