0effac771db3bdd91c0ba5977705f359da28ec2b
Glavyanov <dimitarglavianov@gmail.com>
2021-11-18 22:21:12 +0200

    Contains Solution for problem in Inheritance - Lab

---
+++ OOP_C#/01.Inheritance-Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty.");
+            }
+            Random rnd = new Random();
+            int index = rnd.Next(0, this.Count - 1);
+            string temp = this[index];
+            this.RemoveAt(index);
+            return temp;
+        }
+    }
+}
---
+++ OOP_C#/01.Inheritance-Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList rndList = new RandomList() { "ab", "cd", "ef", "gh" };
+            Console.WriteLine(string.Join(' ',rndList));
+            rndList.RandomString();
+            Console.WriteLine(string.Join(' ',rndList));
+        }
+    }
+}
