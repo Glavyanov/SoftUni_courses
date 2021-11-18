f1fd4cc88481493266810b8656d126cf8897aefc
Glavyanov <dimitarglavianov@gmail.com>
2021-11-18 22:22:29 +0200

    Contains Solution for problem in Inheritance - Lab

---
+++ OOP_C#/01.Inheritance-Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> items)
+        {
+            foreach (var item in items)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+    }
+}
---
+++ OOP_C#/01.Inheritance-Lab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
