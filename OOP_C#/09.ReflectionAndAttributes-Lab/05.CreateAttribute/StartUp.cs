5cfc07696db6a5b05c175e9405cb77b66a6f34a4
Glavyanov <dimitarglavianov@gmail.com>
2021-11-19 09:34:19 +0200

    Contains solution for problem in Reflection and Attributes - Lab

---
+++ OOP_C#/09.ReflectionAndAttributes-Lab/05.CreateAttribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    public class StartUp
+    {
+        [Author("George")]
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
