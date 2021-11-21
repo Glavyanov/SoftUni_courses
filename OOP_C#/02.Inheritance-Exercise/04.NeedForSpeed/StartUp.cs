e2bc0bfc1a27f1e6efc0c00a1cd596d0e2c07de0
Glavyanov <dimitarglavianov@gmail.com>
2021-11-21 22:17:24 +0200

    Contains Solution for problem in Inheritance - Exercise

---
+++ OOP_C#/02.Inheritance-Exercise/04.NeedForSpeed/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SportCar sport = new SportCar(200, 10);
+            Console.WriteLine(sport.Fuel);
+            sport.Drive(1);
+            Console.WriteLine(sport.Fuel);
+            Car car = new Car(200, 9);
+            Console.WriteLine(car.Fuel);
+            car.Drive(1);
+            Console.WriteLine(car.Fuel);
+        }
+    }
+}
---
+++ OOP_C#/02.Inheritance-Exercise/04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+        
+        private int horsePower;
+
+        private double fuel;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public int HorsePower { get; set; }
+
+        public double Fuel { get; set; }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= this.FuelConsumption * kilometers;
+        }
+
+    }
+}
