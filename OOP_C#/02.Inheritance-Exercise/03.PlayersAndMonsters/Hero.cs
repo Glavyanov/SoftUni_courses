2096775ee1f4043f48484b5b170ee850402e84a1
Glavyanov <dimitarglavianov@gmail.com>
2021-11-21 22:15:39 +0200

    Contains Solution for problem in Inheritance - Exercise

---
+++ OOP_C#/02.Inheritance-Exercise/03.PlayersAndMonsters/Hero.cs
@@ -0,0 +1,20 @@
+﻿namespace PlayersAndMonsters
+{
+    public abstract class Hero
+    {
+        public Hero(string username, int level)
+        {
+            this.UserName = username;
+            this.Level = level;
+        }
+
+        public string UserName { get; set; }
+
+        public int Level { get; set; }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.UserName} Level: {this.Level}";
+        }
+    }
+}
