6e762207021f86aba34135bdc5f18c5df5d32e80
Glavyanov <dimitarglavianov@gmail.com>
2021-12-01 21:14:10 +0200

    Contains solution for problem in Interfaces and Abstraction-Exercise

---
+++ OOP_C#/06.InterfacesAndAbstraction-Exercise/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+    }
+}
