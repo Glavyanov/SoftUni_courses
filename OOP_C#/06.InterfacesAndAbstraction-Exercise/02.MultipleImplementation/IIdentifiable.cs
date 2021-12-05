b886cfd23ef616d2870a8941a9da5d1a889f7438
Glavyanov <dimitarglavianov@gmail.com>
2021-12-05 19:15:15 +0200

    Contains solution for problem in Interfaces and Abstraction-Exercise

---
+++ OOP_C#/06.InterfacesAndAbstraction-Exercise/02.MultipleImplementation/IIdentifiable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public interface IIdentifiable
+    {
+        public string Id { get; }
+
+    }
+}
