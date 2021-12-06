cc7ed5055408061d724bb418394fc7fecd21a024
Glavyanov <dimitarglavianov@gmail.com>
2021-12-06 19:53:26 +0200

    Contains solution for problem in C# OOP Retake Exam - 22August2020__01.Structure

---
+++ OOP_C#/OldExams/C#OOPRetakeExam-22August2020/01.Structure/Repositories/Entities/CarRepository.cs
@@ -0,0 +1,16 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class CarRepository : Repository<ICar>
+    {
+        public override ICar GetByName(string name)
+        {
+            return this.collection.FirstOrDefault(c => c.Model == name);
+        }
+    }
+}
---
+++ OOP_C#/OldExams/C#OOPRetakeExam-22August2020/01.Structure/Repositories/Entities/RaceRepository.cs
@@ -0,0 +1,16 @@
+﻿using EasterRaces.Models.Races.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class RaceRepository : Repository<IRace>
+    {
+        public override IRace GetByName(string name)
+        {
+            return base.collection.FirstOrDefault( r=> r.Name == name);
+        }
+    }
+}
