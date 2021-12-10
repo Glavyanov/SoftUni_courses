6171ec73c2c2953e7ae872334ecfd78fdc8a11b9
Glavyanov <dimitarglavianov@gmail.com>
2021-12-10 19:53:59 +0200

    Contains solution for problem in Polymorphism - Lab

---
+++ OOP_C#/07.Polymorphism-Lab/02.Animals/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+        public string Name { get; private set; }
+               
+        public string FavouriteFood { get; private set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+           
+        }
+    }
+}
