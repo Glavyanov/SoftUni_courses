242217d910af331a1669f385331e9858b1d08d18
Glavyanov <dimitarglavianov@gmail.com>
2021-12-26 19:40:57 +0200

    Contains solution for problem in Syntax, Functions and Statements - Lab

---
+++ JS_Advanced/01.SyntaxFunctionsAndStatements-Lab/07.dayOfWeek.js
@@ -0,0 +1,32 @@
+function solve(input){
+    let result;
+    input = String(input);
+    switch (input) {
+        case'Monday':
+            result = 1;
+            break;
+        case 'Tuesday':
+            result = 2;
+            break;
+        case 'Wednesday':
+            result = 3;
+            break;
+        case 'Thursday':
+            result = 4;
+            break;
+        case 'Friday':
+            result = 5;
+            break;
+        case 'Saturday':
+            result = 6;
+            break;
+        case 'Sunday':
+            result = 7;
+            break;
+    
+        default:
+            result = 'error';
+            break;
+    }
+    return result;
+}
