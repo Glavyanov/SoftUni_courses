c03f87c843191fb25d2bff95fe441b584a1e5f41
Glavyanov <dimitarglavianov@gmail.com>
2022-01-13 19:11:09 +0200

    Contains solution for problem in Syntax, Functions and Statements - Exercise

--- JS_Advanced/02.SyntaxFunctionsAndStatements-Exercise/03.sameNumbers.js
+++ JS_Advanced/02.SyntaxFunctionsAndStatements-Exercise/03.sameNumbers.js
@@ -1,7 +1,7 @@
 function solve(num) {
     let isSame = true;
     let sum = 0;
-    numString = String(num);
+    let numString = String(num);
     for (let index = numString.length - 1; index > 0; index--) {
         if (numString[index] != numString[index - 1]) {
             isSame = false;
