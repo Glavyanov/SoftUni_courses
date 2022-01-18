6e070e03e3b7835ebc0eda2c9a6589071fa1f3fb
Glavyanov <dimitarglavianov@gmail.com>
2022-01-19 00:32:22 +0200

    Contains solution for problem in DOM Introduction-Lab

---
+++ JS_Advanced/07.DOM-Introduction-Lab/04.showMore/showMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let element = document.getElementById('more');
+    let text = document.getElementById('text');
+    element.style.display = 'none';
+    text.style.display = 'inline';
+}
