c44b58e3e7b9b7c66a1c06b934c65c5d31e7209e
Glavyanov <dimitarglavianov@gmail.com>
2022-01-20 12:49:02 +0200

    Contains solution for problem in DOM Introduction-Exercise

---
+++ JS_Advanced/08.DOM-Introduction-Exercise/01.subtraction/subtract.js
@@ -0,0 +1,5 @@
+function subtract() {
+    let first = Number(document.getElementById('firstNumber').value);
+    let second = Number(document.getElementById('secondNumber').value);
+    document.getElementById('result').innerHTML = first - second;
+}
