aea652d177af9c63191c991d999bc4f1b51660bc
Glavyanov <dimitarglavianov@gmail.com>
2022-01-21 13:45:39 +0200

    Contains solution for problem in DOM Introduction-Exercise

---
+++ JS_Advanced/08.DOM-Introduction-Exercise/02.pascalOrCamelCase/solution.js
@@ -0,0 +1,28 @@
+function solve() {
+  let wordsToPerform = document.getElementById('text').value;
+  let template = document.getElementById('naming-convention').value;
+  let result = '';
+  if (template === 'Camel Case') {
+    result = wordsToPerform.split(' ').reduce((acc, el, i) => {
+      i ? acc += el[0].toUpperCase() + el.substring(1).toLowerCase() : acc += el.toLowerCase();
+      return acc;
+    }, '');
+  } else if (template === 'Pascal Case') {
+    result = wordsToPerform.split(' ').reduce((acc, el) => {
+      acc += el[0].toUpperCase() + el.substring(1).toLowerCase();
+      return acc;
+    }, '');
+  } else {
+    result = 'Error!';
+  }
+  document.getElementById('result').innerHTML = result;
+
+}
+let list = {
+  collection: [],
+  add(){
+    collection.push(2);
+  }
+}
+list.add();
+console.log(list.collection.join(' '));
