9e22f4f1f313f40a457a3cb083af06634a98de30
Glavyanov <dimitarglavianov@gmail.com>
2022-01-22 11:36:49 +0200

    Contains solution for problem in DOM Introduction-Exercise

---
+++ JS_Advanced/08.DOM-Introduction-Exercise/04.searchInList/search.js
@@ -0,0 +1,18 @@
+function search() {
+   let [...towns] = document.querySelectorAll('#towns li');
+   let search = document.querySelector('#searchText').value;
+   let count = 0;
+   
+   towns.forEach(x  => {
+      if (x.textContent.includes(search)) {
+         x.style.fontWeight = 'bold';
+         x.style.textDecoration = 'underline';
+         count++;
+      }else{
+         x.style.fontWeight = 'normal';
+         x.style.textDecoration = 'none';
+         
+      }
+   });
+   count ? document.querySelector('#result').textContent = `${count} matches found`:document.querySelector('#result').textContent = '';
+}
