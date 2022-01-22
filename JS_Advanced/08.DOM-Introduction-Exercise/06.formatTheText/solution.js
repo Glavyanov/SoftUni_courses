eb29a4dc67bc32fd1571d833b31a9d5aec514976
Glavyanov <dimitarglavianov@gmail.com>
2022-01-22 20:13:52 +0200

    Contains solution for problem in DOM Introduction-Exercise

---
+++ JS_Advanced/08.DOM-Introduction-Exercise/06.formatTheText/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+  let textToPtag = document.querySelector('#input').value;
+  textToPtag = textToPtag.split('').map(x=> escapeHtml(x)).join('').split('.').filter(x => x).map(x => x + '. ');
+  document.querySelector('#output').innerHTML = '';
+  while(textToPtag.length){
+    let current = textToPtag.splice(0,3).join('').trimEnd();
+    document.querySelector('#output').innerHTML += `<p>${current}</p>`;
+  }
+  
+  function escapeHtml(value) {
+    return value
+      .toString()
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;');
+  }
+}
