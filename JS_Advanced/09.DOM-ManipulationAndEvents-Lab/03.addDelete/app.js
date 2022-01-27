30f8b5ae0dc42460606c743ceb600c41bc1fb751
Glavyanov <dimitarglavianov@gmail.com>
2022-01-27 12:09:01 +0200

    Contains solution for problem in DOM Manipulations and Events - Lab

---
+++ JS_Advanced/09.DOM-ManipulationAndEvents-Lab/03.addDelete/app.js
@@ -0,0 +1,18 @@
+function addItem() {
+    let text = document.getElementById('newItemText').value;
+    let ulElement = document.getElementById('items');
+    let liElement = document.createElement('li');
+    let aElement = document.createElement('a');
+    aElement.href = '#';
+    aElement.textContent = '[Delete]';
+    liElement.textContent = text;
+    liElement.appendChild(aElement);
+    aElement.addEventListener('click', (e) => {
+        console.log(e.currentTarget.parentNode);
+        e.currentTarget.parentNode.remove();
+    });
+    document.getElementById('newItemText').value = '';
+    
+    text ? text.search(/^\s*$/) == -1 ? ulElement.appendChild(liElement) : null : null;
+
+}
