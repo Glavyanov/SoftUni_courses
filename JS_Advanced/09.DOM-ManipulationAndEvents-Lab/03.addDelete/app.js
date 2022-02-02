ef9a4467693c483ecf037236d3fa973d0767f8dc
Glavyanov <dimitarglavianov@gmail.com>
2022-02-02 21:26:43 +0200

    Contains solution for problem in DOM Manipulations and Events - Lab

--- JS_Advanced/09.DOM-ManipulationAndEvents-Lab/03.addDelete/app.js
+++ JS_Advanced/09.DOM-ManipulationAndEvents-Lab/03.addDelete/app.js
@@ -8,11 +8,10 @@ function addItem() {
     liElement.textContent = text;
     liElement.appendChild(aElement);
     aElement.addEventListener('click', (e) => {
-        console.log(e.currentTarget.parentNode);
         e.currentTarget.parentNode.remove();
     });
     document.getElementById('newItemText').value = '';
-    
+
     text ? text.search(/^\s*$/) == -1 ? ulElement.appendChild(liElement) : null : null;
 
 }
