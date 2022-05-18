4c5a973e0b4dc8e6e9163eb5c6c079a619ba811e
Glavyanov <dimitarglavianov@gmail.com>
2022-05-19 01:47:43 +0300

    Exercises Database Introduction from 7 to 10 task

--- DB_C#/01.MS_SQL/01.DatabasesIntroduction/AllExercisesDatabaseIntroduction.sql
+++ DB_C#/01.MS_SQL/01.DatabasesIntroduction/AllExercisesDatabaseIntroduction.sql
@@ -55,3 +55,32 @@ INSERT INTO [People]([Name],[Picture],[Height],[Weight],[Gender],[Birthdate],[Bi
 (N'Борис',CAST('0xBDC5C000000000000000000000000000' As varbinary(MAX)),1.8,80.3,'m','01.01.1983',N'Някаква биография'),
 (N'Борислава',CAST('0xBDC5C000000000000000000000000000' As varbinary(MAX)),1.8,80.3,'f','01.01.1982',N'Някаква биография'),
 (N'Георги',CAST('0xBDC5C000000000000000000000000000' As varbinary(MAX)),1.8,80.3,'m','01.01.1981',N'Някаква биография')
+
+--Problem 8.	Create Table Users
+CREATE TABLE [Users](
+	[Id] BIGINT PRIMARY KEY IDENTITY,
+	[Username] VARCHAR(30) NOT NULL,
+	[Password] VARCHAR(26) NOT NULL,
+	[ProfilePicture] VARBINARY(MAX),
+	CHECK (DATALENGTH([ProfilePicture])<=900000),
+	[LastLoginTime] DATETIME2,
+	[IsDeleted] BIT
+);
+
+INSERT INTO [Users]([Username],[Password],[ProfilePicture],[LastLoginTime],[IsDeleted]) VALUES 
+('User1','1',NULL,'01.01.2009',0),
+('User2','2',NULL,'01.01.2019',0),
+('User3','3',NULL,'01.01.2015',1),
+('User4','4',NULL,'01.01.2008',0),
+('User5','5',NULL,'01.01.2021',1)
+
+--Problem 9.	Change Primary Key
+ALTER TABLE [Users]
+DROP CONSTRAINT [PK__Users__3214EC07AA8EC090]
+
+ALTER TABLE [USers]
+ADD CONSTRAINT [PK_UsersComposite_IdAndUsername] PRIMARY KEY ([Id],[Username])
+
+--Problem 10.	Add Check Constraint
+ALTER TABLE [Users]
+ADD CONSTRAINT [PasswordLength] CHECK (LEN([Password])>4) 
