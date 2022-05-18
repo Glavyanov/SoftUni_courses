c5b884863ba63d662b406216daffa46a9037a914
Glavyanov <dimitarglavianov@gmail.com>
2022-05-18 23:50:30 +0300

    Exercises Database Introduction from 5 to 7 task

--- DB_C#/01.MS_SQL/01.DatabasesIntroduction/AllExercisesDatabaseIntroduction.sql
+++ DB_C#/01.MS_SQL/01.DatabasesIntroduction/AllExercisesDatabaseIntroduction.sql
@@ -28,4 +28,30 @@ INSERT INTO [Towns]([Id], [Name]) VALUES
 INSERT INTO [Minions]([Id], [Name], [Age], [TownId]) VALUES 
 (1,'Kevin',22,1),
 (2,'Bob',15,3),
-(3,'Steward',NULL,2)	      
+(3,'Steward',NULL,2)
+
+--Problem 5.	Truncate Table Minions
+TRUNCATE TABLE [Minions];
+
+--Problem 6.	Drop All Tables
+DROP TABLE [MInions],[Towns];
+
+--Problem 7.	Create Table People
+CREATE TABLE [People](
+	[Id] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] NVARCHAR(200) NOT NULL,
+	[Picture] VARBINARY(MAX),
+	CHECK (DATALENGTH([Picture])<=20000000),
+	[Height] DECIMAL(4,2),
+	[Weight] DECIMAL(5,2),
+	[Gender] CHAR NOT NULL,
+	[Birthdate] DATETIME2 NOT NULL,
+	[Biography] NVARCHAR(MAX)
+);
+
+INSERT INTO [People]([Name],[Picture],[Height],[Weight],[Gender],[Birthdate],[Biography]) VALUES
+(N'Александър',CAST('0xBDC5C000000000000000000000000000' As varbinary(MAX)),1.8,8.3,'m','02.02.1990',N'Някаква биография'),
+(N'Александра',CAST('0xBDC5C000000000000000000000000000' As varbinary(MAX)),1.8,80.3,'f','01.01.1984',N'Някаква биография'),
+(N'Борис',CAST('0xBDC5C000000000000000000000000000' As varbinary(MAX)),1.8,80.3,'m','01.01.1983',N'Някаква биография'),
+(N'Борислава',CAST('0xBDC5C000000000000000000000000000' As varbinary(MAX)),1.8,80.3,'f','01.01.1982',N'Някаква биография'),
+(N'Георги',CAST('0xBDC5C000000000000000000000000000' As varbinary(MAX)),1.8,80.3,'m','01.01.1981',N'Някаква биография')
