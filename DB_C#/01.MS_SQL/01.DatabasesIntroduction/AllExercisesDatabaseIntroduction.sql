52e9244f1f949b91f6da37038d8507a1142f9938
Glavyanov <dimitarglavianov@gmail.com>
2022-05-22 22:14:00 +0300

    Exercises Database Introduction 20 and 21 tasks

--- DB_C#/01.MS_SQL/01.DatabasesIntroduction/AllExercisesDatabaseIntroduction.sql
+++ DB_C#/01.MS_SQL/01.DatabasesIntroduction/AllExercisesDatabaseIntroduction.sql
@@ -426,3 +426,32 @@ INSERT INTO [Occupancies]([EmployeeId],[DateOccupied],[AccountNumber],[RoomNumbe
 SELECT * FROM [Towns]
 SELECT * FROM [Departments]
 SELECT * FROM [Employees]
+
+--Problem 20.	Basic Select All Fields and Order Them
+ SELECT * 
+   FROM [Towns]
+ORDER BY[Name]
+ 
+ SELECT * 
+   FROM [Departments]
+ORDER BY[Name]
+
+ SELECT * 
+   FROM [Employees]
+ORDER BY[Salary] DESC
+
+--Problem 21.	Basic Select Some Fields
+ SELECT [Name] 
+   FROM [Towns]
+ORDER BY[Name]
+
+  SELECT[Name] 
+   FROM [Departments]
+ORDER BY[Name]
+
+ SELECT [FirstName]
+       ,[LastName]
+	   ,[JobTitle]
+	   ,[Salary]
+   FROM [Employees]
+ORDER BY[Salary] DESC
