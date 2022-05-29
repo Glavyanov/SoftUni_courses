c31d2f2795c6aad0b7fa36fee16d377ba064ab78
Glavyanov <dimitarglavianov@gmail.com>
2022-05-29 21:10:25 +0300

    Built-inFunctions-Exercise-Problem 2

--- DB_C#/01.MS_SQL/04.Built-inFunctions/Built-inFunctions-Exercise.sql
+++ DB_C#/01.MS_SQL/04.Built-inFunctions/Built-inFunctions-Exercise.sql
@@ -3,3 +3,15 @@ SELECT [FirstName]
       ,[LastName]
   FROM [Employees]
  WHERE [FirstName] LIKE 'Sa%'
+
+--Problem 2.	Find Names of All employees by Last Name
+SELECT [FirstName]
+      ,[LastName] 
+  FROM [Employees]
+ WHERE [LastName] LIKE'%EI%'
+ 
+ --Problem 2.0.
+SELECT [FirstName]
+      ,[LastName] 
+  FROM [Employees]
+ WHERE CHARINDEX('EI', [LastName]) <> 0 -- => case-insensitive
