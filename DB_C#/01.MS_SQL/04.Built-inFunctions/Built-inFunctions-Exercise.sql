b71ebef0c95bbabf92f8d1f7eb113ecf37c0561c
Glavyanov <dimitarglavianov@gmail.com>
2022-05-30 23:43:13 +0300

    Built-inFunctions-Exercise-Problem from 8 to 12

--- DB_C#/01.MS_SQL/04.Built-inFunctions/Built-inFunctions-Exercise.sql
+++ DB_C#/01.MS_SQL/04.Built-inFunctions/Built-inFunctions-Exercise.sql
@@ -1,3 +1,5 @@
+--Part I – Queries for SoftUni Database
+
 --Problem 1.	Find Names of All Employees by First Name
 SELECT [FirstName]
       ,[LastName]
@@ -98,3 +100,51 @@ ORDER BY [Name]
     FROM [Towns]
    WHERE [Name] LIKE '[^rdb]%'
 ORDER BY [Name]
+
+--Problem 8.	Create View Employees Hired After 2000 Year
+CREATE OR ALTER VIEW [V_EmployeesHiredAfter2000] AS 
+	 SELECT [FirstName]
+		   ,[LastName]
+	   FROM [Employees]
+	  WHERE YEAR([HireDate]) > 2000
+
+--Problem 9.	Length of Last Name
+SELECT [FirstName]
+      ,[LastName]
+  FROM [Employees]
+ WHERE LEN([LastName]) = 5
+
+ --Problem 10.	Rank Employees by Salary
+  SELECT [EmployeeID]
+	    ,[FirstName]
+	    ,[LastName]
+        ,[Salary]
+		,DENSE_RANK() OVER (PARTITION BY[Salary] ORDER BY [EmployeeID]) AS [Rank]
+    FROM [Employees]
+   WHERE [Salary] BETWEEN 10000 AND 50000
+ORDER BY [Salary] DESC
+
+--Problem 11.	Find All Employees with Rank 2
+SELECT * 
+  FROM 
+     (
+	   SELECT [EmployeeID]
+	  		 ,[FirstName]
+	   	   	 ,[LastName]
+	  		 ,[Salary]
+	  		 ,DENSE_RANK() OVER (PARTITION BY[Salary] ORDER BY [EmployeeID]) AS [Rank]
+	  	 FROM [Employees]
+	    WHERE [Salary] BETWEEN 10000 AND 50000 
+     )
+	  AS [Inner Select]
+   WHERE [RANK] = 2
+ORDER BY [Salary] DESC
+
+--Part II – Queries for Geography Database 
+
+--Problem 12.	Countries Holding ‘A’ 3 or More Times
+SELECT [CountryName]
+	  ,[IsoCode] AS [ISO Code]
+FROM [Countries]
+WHERE [CountryName] LIKE '%a%a%a%'
+ORDER BY[IsoCode]
