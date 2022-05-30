cfa93767c07a5f19b1786fe12f6f78104ac6262b
Glavyanov <dimitarglavianov@gmail.com>
2022-05-30 23:49:27 +0300

    Built-inFunctions-Exercise-Problem from 8 to 12

--- DB_C#/01.MS_SQL/04.Built-inFunctions/Built-inFunctions-Exercise.sql
+++ DB_C#/01.MS_SQL/04.Built-inFunctions/Built-inFunctions-Exercise.sql
@@ -129,11 +129,11 @@ SELECT *
   FROM 
      (
 	   SELECT [EmployeeID]
-	  		 ,[FirstName]
-	   	   	 ,[LastName]
-	  		 ,[Salary]
-	  		 ,DENSE_RANK() OVER (PARTITION BY[Salary] ORDER BY [EmployeeID]) AS [Rank]
-	  	 FROM [Employees]
+             ,[FirstName]
+             ,[LastName]
+             ,[Salary]
+             ,DENSE_RANK() OVER (PARTITION BY[Salary] ORDER BY [EmployeeID]) AS [Rank]
+         FROM [Employees]
 	    WHERE [Salary] BETWEEN 10000 AND 50000 
      )
 	  AS [Inner Select]
