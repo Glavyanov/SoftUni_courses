574d8d850797e3cfeb6e461c809e06a84190a72a
Glavyanov <dimitarglavianov@gmail.com>
2022-06-01 21:13:53 +0300

    SubqueriesAndJoins-Exercise Problems from 4 to 8

--- DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
+++ DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
@@ -39,3 +39,55 @@ ORDER BY e.[EmployeeID]
     JOIN [Departments] AS d ON e.[DepartmentID] = d.[DepartmentID] 
    WHERE d.[Name] IN ('Sales')
 ORDER BY e.[EmployeeID]
+
+--Problem 4.   Employee Departments
+  SELECT TOP 5
+         e.[EmployeeID]
+        ,e.[FirstName]
+        ,e.[Salary]
+        ,d.[Name] AS [DepartmentName]
+    FROM [Employees] AS e
+    JOIN [Departments] AS d ON e.[DepartmentID] = d.[DepartmentID]
+ORDER BY d.[DepartmentID]
+
+--Problem 5.	Employees Without Project
+   SELECT TOP 3
+          e.[EmployeeID]
+         ,e.[FirstName]
+     FROM [Employees] AS e
+LEFT JOIN [EmployeesProjects] AS ep ON e.[EmployeeID] = ep.[EmployeeID]
+    WHERE ep.[EmployeeID] IS NULL
+ ORDER BY e.[EmployeeID]
+
+--Problem 6.   Employees Hired After
+  SELECT e.[FirstName]
+        ,e.[LastName]
+        ,e.[HireDate]
+        ,d.[Name] AS [DepartmentName]
+    FROM [Employees] AS e
+    JOIN [Departments] AS d ON e.[DepartmentID] = d.[DepartmentID]
+   WHERE e.[HireDate] > '1999-01-01' AND d.[Name] IN ('Sales','Finance')
+ORDER BY e.[HireDate]
+
+--Problem 7.  	Employees with Project
+  SELECT TOP 5 
+         e.[EmployeeID]
+        ,e.[FirstName]
+        ,p.[Name] AS [ProjectName]
+    FROM [Employees] AS e
+    JOIN [EmployeesProjects] AS ep ON e.[EmployeeID] = ep.[EmployeeID]
+    JOIN [Projects] AS p ON ep.[ProjectID] = p.[ProjectID]
+   WHERE p.[StartDate] > '2002-08-13' AND p.EndDate IS NULL
+ORDER BY e.[EmployeeID]
+
+--Problem 8.  	Employee 24
+SELECT TOP 5 
+       e.[EmployeeID]
+      ,e.[FirstName]
+      ,CASE
+		   WHEN YEAR(p.[StartDate]) < 2005 THEN p.[Name]
+		   ELSE NULL
+       END AS [ProjectName]
+  FROM [Employees] AS e
+  JOIN [EmployeesProjects] AS ep ON e.[EmployeeID] = ep.[EmployeeID]
+  JOIN [Projects] AS p ON ep.[ProjectID] = p.[ProjectID] AND e.[EmployeeID] = 24
