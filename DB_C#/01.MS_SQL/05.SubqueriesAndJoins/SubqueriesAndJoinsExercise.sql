972e616783092f9a65e782b12891131cd7ffb078
Glavyanov <dimitarglavianov@gmail.com>
2022-06-02 23:16:28 +0300

    SubqueriesAndJoins-Exercise Problem 10

--- DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
+++ DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
@@ -101,3 +101,14 @@ SELECT TOP 5
     JOIN [Employees] AS m ON e.[ManagerID] = m.[EmployeeID]
    WHERE e.[ManagerID] IN (3,7)
 ORDER BY e.[EmployeeID]
+
+--Problem 10. Employee Summary
+  SELECT TOP 50
+        e.[EmployeeID]
+        ,CONCAT(e.[FirstName],' ',e.[LastName]) AS [EmployeeName]
+        ,CONCAT(m.[FirstName],' ' ,m.[LastName]) AS [ManagerName]
+        ,d.[Name] AS [DepartmentName]
+    FROM [Employees] AS e
+    JOIN [Employees] AS m ON e.[ManagerID] = m.[EmployeeID]
+    JOIN [Departments] AS d ON e.[DepartmentID] = d.DepartmentID
+ORDER BY e.[EmployeeID]
