63642430495ef8b7c65588a400618df2f11dd49f
Glavyanov <dimitarglavianov@gmail.com>
2022-06-02 23:37:46 +0300

    SubqueriesAndJoins-Exercise Problem 12

--- DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
+++ DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
@@ -122,3 +122,17 @@ FROM (
      GROUP BY [DepartmentID]
      ) 
   AS [avQ]
+
+--Part II – Queries for Geography Database
+
+--Problem 12. Highest Peaks in Bulgaria
+  SELECT c.[CountryCode]
+        ,m.[MountainRange]
+        ,p.[PeakName]
+        ,p.[Elevation]
+    FROM [Countries] AS c
+    JOIN [MountainsCountries] AS mc ON c.[CountryCode] = mc.[CountryCode]
+    JOIN [Mountains] AS m ON mc.[MountainId] = m.[Id]
+    JOIN [Peaks] AS p ON m.Id = p.MountainId
+WHERE c.[CountryCode] = 'BG' AND p.[Elevation] > 2835
+ORDER BY p.[Elevation] DESC
