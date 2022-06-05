816eaf0e11e9df29d77441a5bcf2b9b11d8ed51d
Glavyanov <dimitarglavianov@gmail.com>
2022-06-05 11:09:18 +0300

    SubqueriesAndJoins-Exercise Problems 17 and 18

--- DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
+++ DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
@@ -182,3 +182,51 @@ ORDER BY [MaxCurrency].[ContinentCode]
      FROM [Countries] AS c
 LEFT JOIN [MountainsCountries] AS mc ON c.[CountryCode] = mc.[CountryCode]
     WHERE mc.[MountainId] IS NULL
+
+--Problem 17. Highest Peak and Longest River by Country
+   SELECT TOP 5
+          [CountryName]
+         ,MAX(p.[Elevation]) AS [HighestPeakElevation]
+         ,MAX(r.[Length]) AS [LongestRiverLength]
+     FROM [Countries] AS c
+LEFT JOIN [MountainsCountries] AS mc ON c.[CountryCode] = mc.[CountryCode]
+LEFT JOIN [Mountains] AS m ON mc.[MountainId] = m.Id
+LEFT JOIN [Peaks] AS p ON m.[Id] = p.[MountainId]
+LEFT JOIN [CountriesRivers] AS cr ON c.[CountryCode] = cr.[CountryCode]
+LEFT JOIN [Rivers] AS r ON cr.[RiverId] = r.[Id]
+ GROUP BY c.[CountryName]
+ ORDER BY [HighestPeakElevation] DESC,[LongestRiverLength] DESC, [CountryName]
+
+--Problem 18. Highest Peak Name and Elevation by Country
+  SELECT TOP 5
+         [CountryName] AS [Country]
+        ,CASE
+			WHEN [PeakName] IS NULL THEN '(no highest peak)'
+			ELSE [PeakName]
+         END AS [Highest Peak Name]
+        ,CASE
+			WHEN [Highest Peak Elevation] IS NULL THEN 0
+			ELSE [Highest Peak Elevation]
+         END AS [Highest Peak Elevation]
+        ,CASE
+			WHEN [MountainRange] IS NULL THEN '(no mountain)'
+			ELSE [MountainRange]
+         END AS [Mountain]
+    FROM (
+			SELECT *
+                  ,DENSE_RANK() OVER (PARTITION BY [CountryName] ORDER BY [Highest Peak Elevation] DESC) AS [RankElev]
+              FROM (
+                           SELECT c.[CountryName]
+                                 ,p.[PeakName]
+                                 ,p.[Elevation] AS [Highest Peak Elevation]
+                                 ,m.[MountainRange]
+                             FROM [Countries] AS c
+						LEFT JOIN [MountainsCountries] AS mc ON c.[CountryCode] = mc.[CountryCode]
+						LEFT JOIN [Mountains] AS m ON mc.[MountainId] =  m.[Id]
+						LEFT JOIN [Peaks] AS p ON m.[Id] = p.[MountainId]
+                    ) 
+                 AS [AllInfo]
+         ) 
+      AS [CurrentRank]
+   WHERE [RankElev] = 1
+ORDER BY [CountryName],[Highest Peak Name]
