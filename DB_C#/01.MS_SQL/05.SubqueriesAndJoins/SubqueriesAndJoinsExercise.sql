26dabbbd19f6dc1470d6ceb944f3a1bdc1d3938f
Glavyanov <dimitarglavianov@gmail.com>
2022-06-05 11:12:13 +0300

    Update problems 17 and 18

--- DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
+++ DB_C#/01.MS_SQL/05.SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
@@ -201,19 +201,19 @@ LEFT JOIN [Rivers] AS r ON cr.[RiverId] = r.[Id]
   SELECT TOP 5
          [CountryName] AS [Country]
         ,CASE
-			WHEN [PeakName] IS NULL THEN '(no highest peak)'
-			ELSE [PeakName]
+             WHEN [PeakName] IS NULL THEN '(no highest peak)'
+             ELSE [PeakName]
          END AS [Highest Peak Name]
         ,CASE
-			WHEN [Highest Peak Elevation] IS NULL THEN 0
-			ELSE [Highest Peak Elevation]
+             WHEN [Highest Peak Elevation] IS NULL THEN 0
+             ELSE [Highest Peak Elevation]
          END AS [Highest Peak Elevation]
         ,CASE
-			WHEN [MountainRange] IS NULL THEN '(no mountain)'
-			ELSE [MountainRange]
+             WHEN [MountainRange] IS NULL THEN '(no mountain)'
+             ELSE [MountainRange]
          END AS [Mountain]
     FROM (
-			SELECT *
+            SELECT *
                   ,DENSE_RANK() OVER (PARTITION BY [CountryName] ORDER BY [Highest Peak Elevation] DESC) AS [RankElev]
               FROM (
                            SELECT c.[CountryName]
@@ -221,9 +221,9 @@ LEFT JOIN [Rivers] AS r ON cr.[RiverId] = r.[Id]
                                  ,p.[Elevation] AS [Highest Peak Elevation]
                                  ,m.[MountainRange]
                              FROM [Countries] AS c
-						LEFT JOIN [MountainsCountries] AS mc ON c.[CountryCode] = mc.[CountryCode]
-						LEFT JOIN [Mountains] AS m ON mc.[MountainId] =  m.[Id]
-						LEFT JOIN [Peaks] AS p ON m.[Id] = p.[MountainId]
+                        LEFT JOIN [MountainsCountries] AS mc ON c.[CountryCode] = mc.[CountryCode]
+                        LEFT JOIN [Mountains] AS m ON mc.[MountainId] =  m.[Id]
+                        LEFT JOIN [Peaks] AS p ON m.[Id] = p.[MountainId]
                     ) 
                  AS [AllInfo]
          ) 
