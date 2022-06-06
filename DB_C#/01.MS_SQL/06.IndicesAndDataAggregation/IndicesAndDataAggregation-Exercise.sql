c8df1079fcabd61c8d557be9fc148b50da1cbf39
Glavyanov <dimitarglavianov@gmail.com>
2022-06-06 09:20:35 +0300

    IndicesAndDataAggregation-Exercise Problems 2 to 5

--- DB_C#/01.MS_SQL/06.IndicesAndDataAggregation/IndicesAndDataAggregation-Exercise.sql
+++ DB_C#/01.MS_SQL/06.IndicesAndDataAggregation/IndicesAndDataAggregation-Exercise.sql
@@ -3,3 +3,25 @@
 --Problem 1. Records’ Count
 SELECT COUNT([Id]) AS [Count]
   FROM [WizzardDeposits]
+
+--Problem 2. Longest Magic Wand
+SELECT MAX([MagicWandSize]) AS [LongestMagicWand]
+  FROM [WizzardDeposits]
+
+--Problem 3. Longest Magic Wand Per Deposit Groups
+  SELECT [DepositGroup]
+       , MAX([MagicWandSize]) AS [LongestMagicWand]
+    FROM [WizzardDeposits]
+GROUP BY [DepositGroup]
+
+--Problem 4. Smallest Deposit Group Per Magic Wand Size
+  SELECT TOP 2 [DepositGroup]
+    FROM [WizzardDeposits]
+GROUP BY [DepositGroup]
+ORDER BY AVG([MagicWandSize])
+
+--Problem 5. Deposits Sum
+  SELECT [DepositGroup] 
+       , SUM([DepositAmount]) AS [TotalSum]
+    FROM [WizzardDeposits]
+GROUP BY [DepositGroup]
