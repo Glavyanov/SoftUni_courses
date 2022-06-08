82bf8598261b67c6d6ea6fd948c89cce8ca5277e
Glavyanov <dimitarglavianov@gmail.com>
2022-06-08 20:38:48 +0300

    IndicesAndDataAggregation-Exercise Problem 9

--- DB_C#/01.MS_SQL/06.IndicesAndDataAggregation/IndicesAndDataAggregation-Exercise.sql
+++ DB_C#/01.MS_SQL/06.IndicesAndDataAggregation/IndicesAndDataAggregation-Exercise.sql
@@ -49,3 +49,21 @@ ORDER BY [TotalSum] DESC
     FROM [WizzardDeposits]
 GROUP BY [DepositGroup],[MagicWandCreator]
 ORDER BY [MagicWandCreator],[DepositGroup]
+
+--Problem 9. Age Groups
+SELECT [AgeGroup]
+      ,COUNT(*) AS [WizardCount]
+       FROM 
+           ( SELECT 
+                  CASE
+                     WHEN [Age] < 11 THEN '[0-10]'
+                     WHEN [Age] < 21 THEN '[11-20]'
+                     WHEN [Age] < 31 THEN '[21-30]'
+                     WHEN [Age] < 41 THEN '[31-40]'
+                     WHEN [Age] < 51 THEN '[41-50]'
+                     WHEN [Age] < 61 THEN '[51-60]'
+                     WHEN [Age] > 60 THEN '[61+]'
+                  END AS [AgeGroup]
+              FROM [WizzardDeposits] 
+            ) AS [FilterAge]
+GROUP BY [AgeGroup]
