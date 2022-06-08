dbe413335a6ade2f24a47de12f550eac76567a82
Glavyanov <dimitarglavianov@gmail.com>
2022-06-08 20:35:26 +0300

    IndicesAndDataAggregation-Exercise Problem 7

--- DB_C#/01.MS_SQL/06.IndicesAndDataAggregation/IndicesAndDataAggregation-Exercise.sql
+++ DB_C#/01.MS_SQL/06.IndicesAndDataAggregation/IndicesAndDataAggregation-Exercise.sql
@@ -32,3 +32,12 @@ GROUP BY [DepositGroup]
     FROM [WizzardDeposits]
    WHERE [MagicWandCreator] LIKE 'Ollivander%'
 GROUP BY [DepositGroup]
+
+--Problem 7. Deposits Filter
+  SELECT [DepositGroup] 
+       , SUM([DepositAmount]) AS [TotalSum]
+    FROM [WizzardDeposits]
+   WHERE [MagicWandCreator] LIKE 'Ollivander%'
+GROUP BY [DepositGroup]
+  HAVING SUM([DepositAmount]) < 150000
+ORDER BY [TotalSum] DESC
