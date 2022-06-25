d71ab12d3be046374b7d3ba861908b94937ff10c
Glavyanov <dimitarglavianov@gmail.com>
2022-06-25 12:07:41 +0300

    ADO-NET-Exercise-Problem 02VillainNames

---
+++ DB_C#/02.Entity_Framework_Core/ADO-NET-Exercise/02VillainNames/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+
+namespace _02VillainNames
+{
+    public static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        using SqlConnection sqlConnection = new SqlConnection(Config.ConnectionString);
+        sqlConnection.Open();
+
+        string villainsQuery = @"     SELECT v.[Name] 
+                                               , COUNT(mv.MinionId) AS [Count]
+                                            FROM Villains AS v
+                                            JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                                        GROUP BY v.[Name]
+                                          HAVING COUNT(mv.MinionId) > 3
+                                        ORDER BY [Count] DESC";
+
+        SqlCommand villiansNamesCmd = new SqlCommand(villainsQuery, sqlConnection);
+        using SqlDataReader readerVillains = villiansNamesCmd.ExecuteReader();
+        while (readerVillains.Read())
+        {
+            sb.AppendLine($"{readerVillains["Name"]} - {readerVillains["Count"]}");
+        }
+
+        readerVillains.Close();
+        sqlConnection.Close();
+
+        Console.WriteLine(sb.ToString().TrimEnd());
+    }
+}
