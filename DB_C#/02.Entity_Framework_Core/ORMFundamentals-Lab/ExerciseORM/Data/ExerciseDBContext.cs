33a172d9adf869287315f9866af2838830292e96
Glavyanov <dimitarglavianov@gmail.com>
2022-06-29 14:58:57 +0300

    ORMFundamentals-Lab-ORM for Exercise

---
+++ DB_C#/02.Entity_Framework_Core/ORMFundamentals-Lab/ExerciseORM/Data/ExerciseDBContext.cs
@@ -0,0 +1,32 @@
+﻿using ExerciseORM.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExerciseORM.Data
+{
+    public class ExerciseDBContext : DbContext
+    {
+        public ExerciseDBContext()
+        {
+
+        }
+
+        public ExerciseDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Config.ConnectionString);
+            }
+        }
+    }
+}
---
+++ DB_C#/02.Entity_Framework_Core/ORMFundamentals-Lab/ExerciseORM/Data/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExerciseORM.Data.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            //this.Departments = new HashSet<Department>();
+        }
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [MaxLength(50)]
+        public string MiddleName { get; set; }
+
+        [MaxLength(50)]
+        public string JobTitle { get; set; }
+
+        [ForeignKey(nameof(Department))]
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
+
+        //public virtual ICollection<Department> Departments { get; set; }
+    }
+}
