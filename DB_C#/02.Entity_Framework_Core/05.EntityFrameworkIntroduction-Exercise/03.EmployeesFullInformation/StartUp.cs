ef0456959767fffac3ca355ef52c6d7ee61ba419
Glavyanov <dimitarglavianov@gmail.com>
2022-07-03 12:59:40 +0300

    EntityFrameworkIntroduction-Exercise-Problem03

---
+++ DB_C#/02.Entity_Framework_Core/05.EntityFrameworkIntroduction-Exercise/03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(softUniContext));
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var allEmployees = context.Employees
+                                      .OrderBy(e => e.EmployeeId)
+                                      .Select(e => new
+                                      {
+                                          e.FirstName,
+                                          e.LastName,
+                                          e.MiddleName,
+                                          e.JobTitle,
+                                          e.Salary
+                                      })
+                                      .ToArray();
+            foreach (var employee in allEmployees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle}{employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
