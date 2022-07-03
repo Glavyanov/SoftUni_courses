abdbe506f5c6ed3636e495f21cf6416f29cffbf7
Glavyanov <dimitarglavianov@gmail.com>
2022-07-03 12:32:56 +0300

    Update EntityFrameworkIntroduction-Lab

--- DB_C#/02.Entity_Framework_Core/04.EntityFrameworkIntroduction-Lab/Models/SoftUniContext.cs
+++ DB_C#/02.Entity_Framework_Core/04.EntityFrameworkIntroduction-Lab/Models/SoftUniContext.cs
@@ -28,8 +28,8 @@ namespace DemoEFCore.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.;Database=SoftUni;Encrypt=False;User Id=sa; Password=AbabacH4747");
+
+                optionsBuilder.UseSqlServer(Config.ConfigurationString);
             }
         }
 
