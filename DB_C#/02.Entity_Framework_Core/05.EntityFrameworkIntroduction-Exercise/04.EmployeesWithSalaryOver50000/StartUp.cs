0a81ab8aa040f9dc84fe1e78dca9845446fd17b8
Glavyanov <dimitarglavianov@gmail.com>
2022-07-03 13:03:12 +0300

    EntityFrameworkIntroduction-Exercise-Problem04

---
+++ DB_C#/02.Entity_Framework_Core/05.EntityFrameworkIntroduction-Exercise/04.EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(softUniContext)); 
+
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context.Employees
+                                   .Select(e => new
+                                   {
+                                       e.FirstName,
+                                       e.Salary
+                                   })
+                                   .Where(e => e.Salary > 50000)
+                                   .OrderBy(e => e.FirstName)
+                                   .ToArray();
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary:F2}");
+            }                      
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
