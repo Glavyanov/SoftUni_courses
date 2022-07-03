a0394154f7384708c7a1e8cf54f639528de45bbb
Glavyanov <dimitarglavianov@gmail.com>
2022-07-03 12:53:08 +0300

    EntityFrameworkIntroduction-Exercise-Problem02

---
+++ DB_C#/02.Entity_Framework_Core/05.EntityFrameworkIntroduction-Exercise/02.DatabaseFirst/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+
+    }
+}
