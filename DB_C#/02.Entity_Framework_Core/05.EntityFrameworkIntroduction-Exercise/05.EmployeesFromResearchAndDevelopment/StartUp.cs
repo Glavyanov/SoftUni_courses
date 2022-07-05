09161dfddc25033d627c043897cac372eb764b99
Glavyanov <dimitarglavianov@gmail.com>
2022-07-05 07:53:20 +0300

    EntityFrameworkIntroduction-Exercise-Problem05

---
+++ DB_C#/02.Entity_Framework_Core/05.EntityFrameworkIntroduction-Exercise/05.EmployeesFromResearchAndDevelopment/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            Console.WriteLine(GetDepartmentsWithMoreThan5Employees(softUniContext)); 
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employeesFromResearch = context.Employees
+                                               .Select(e => new
+                                               {
+                                                   e.FirstName,
+                                                   e.LastName,
+                                                   DepartmentName = e.Department.Name,
+                                                   e.Salary
+                                               })
+                                               .Where(e => e.DepartmentName == "Research and Development")
+                                               .OrderBy(e => e.Salary)
+                                               .ThenByDescending(e => e.FirstName)
+                                               .ToArray();
+            foreach (var e in employeesFromResearch)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:F2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+     
+}
