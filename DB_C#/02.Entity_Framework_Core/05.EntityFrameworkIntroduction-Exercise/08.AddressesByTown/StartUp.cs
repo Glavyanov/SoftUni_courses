fba55ecdd985004195395cfc568de291e558bb17
Glavyanov <dimitarglavianov@gmail.com>
2022-07-06 07:42:43 +0300

    EntityFrameworkIntroduction-Exercise-Problem08

---
+++ DB_C#/02.Entity_Framework_Core/05.EntityFrameworkIntroduction-Exercise/08.AddressesByTown/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            Console.WriteLine(GetAddressesByTown(softUniContext)); 
+        }
+   
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var topTenAddresses = context.Addresses
+                                         .Select(a => new
+                                         {
+                                             a.AddressText,
+                                             a.Town.Name,
+                                             a.Employees.Count
+
+                                         })
+                                         .OrderByDescending(a => a.Count)
+                                         .ThenBy(a => a.Name)
+                                         .ThenBy(a => a.AddressText)
+                                         .Take(10)
+                                         .ToList();
+            topTenAddresses.ForEach(a =>
+            {
+                sb.AppendLine($"{a.AddressText}, {a.Name} - {a.Count} employees");
+            });
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
