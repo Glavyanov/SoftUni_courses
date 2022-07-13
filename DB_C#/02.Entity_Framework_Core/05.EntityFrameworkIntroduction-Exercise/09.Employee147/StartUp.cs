278c34baf277d93f9757bdd69c08995ec95e64b6
Glavyanov <dimitarglavianov@gmail.com>
2022-07-13 12:38:10 +0300

    EntityFrameworkIntroduction-Exercise-Problem09

---
+++ DB_C#/02.Entity_Framework_Core/05.EntityFrameworkIntroduction-Exercise/09.Employee147/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            Console.WriteLine(GetEmployee147(softUniContext)); 
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employee = context.Employees
+                                  .Select(e => new
+                                  {
+                                      e.EmployeeId,
+                                      e.FirstName,
+                                      e.LastName,
+                                      e.JobTitle,
+                                      Projects = e.EmployeesProjects.Select(p => p.Project.Name).ToArray()
+                                  })
+                                  .FirstOrDefault(e => e.EmployeeId == 147);
+
+            sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+
+            foreach (string project in employee.Projects.OrderBy(p => p))
+            {
+                sb.AppendLine($"{project}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
