bfc77f4d3ad6a34d6aa77c22411a99bee24098a4
Glavyanov <dimitarglavianov@gmail.com>
2022-07-17 19:33:17 +0300

    AdvancedQuerying-Exercise-Problem05

---
+++ DB_C#/02.Entity_Framework_Core/08.AdvancedQuerying-Exercise/05.NotReleasedIn/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            Console.WriteLine(GetBooksNotReleasedIn(db, int.Parse(Console.ReadLine())));
+
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+    }
+}
