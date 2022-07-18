f6567979d4cd1d1ad59d1d2217db12a54bc6d94b
Glavyanov <dimitarglavianov@gmail.com>
2022-07-18 11:24:48 +0300

    AdvancedQuerying-Exercise-Problem09

---
+++ DB_C#/02.Entity_Framework_Core/08.AdvancedQuerying-Exercise/09.BookSearch/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            Console.WriteLine(GetBookTitlesContaining(db, Console.ReadLine()));
+
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            var titles = context.Books
+                .Select(b => b.Title)
+                .ToArray()
+                .Where(b => Regex.IsMatch(b, @$".*{input}.*", RegexOptions.IgnoreCase))
+                .OrderBy(b => b)
+                .ToArray();
+            return string.Join(Environment.NewLine, titles);
+        }
+
+    }
+}
