95f70ef5c6138bef1d9f31c5639d52a483606f93
Glavyanov <dimitarglavianov@gmail.com>
2022-07-18 11:23:06 +0300

    AdvancedQuerying-Exercise-Problem08

---
+++ DB_C#/02.Entity_Framework_Core/08.AdvancedQuerying-Exercise/08.AuthorSearch/BookShop/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            Console.WriteLine(GetAuthorNamesEndingIn(db, Console.ReadLine()));
+
+        }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            var authors = context.Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .Select(a => new
+                {
+                    FullName = $"{a.FirstName} {a.LastName}"
+                })
+                .ToArray();
+
+            foreach (var a in authors.OrderBy(a => a.FullName))
+            {
+                sb.AppendLine(a.FullName);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
