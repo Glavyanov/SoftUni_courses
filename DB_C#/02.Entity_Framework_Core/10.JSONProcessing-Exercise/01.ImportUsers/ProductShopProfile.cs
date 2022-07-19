d8a61056484f29582781612927983f8ccc6a38ca
Glavyanov <dimitarglavianov@gmail.com>
2022-07-19 13:48:02 +0300

    JSONProcessing-Exercise-Problem 01

---
+++ DB_C#/02.Entity_Framework_Core/10.JSONProcessing-Exercise/01.ImportUsers/ProductShopProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProductShop.DTOs.ImportDTOs;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<ImportUserDto, User>();
+        }
+    }
+}
---
+++ DB_C#/02.Entity_Framework_Core/10.JSONProcessing-Exercise/01.ImportUsers/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.ImportDTOs;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        private static string filePath;
+        public static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile(typeof(ProductShopProfile)));
+            ProductShopContext context = new ProductShopContext();
+
+            //Console.WriteLine($"{context.Database.EnsureDeleted()}{Environment.NewLine}{context.Database.EnsureCreated()}");
+
+            InitializeFilePath("users.json");
+            string inputJson = File.ReadAllText(filePath);
+
+            Console.WriteLine(ImportUsers(context, inputJson));
+
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            ImportUserDto[] usersImportDto = JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+            User[] users = Mapper.Map<User[]>(usersImportDto);
+            
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+        private static void InitializeFilePath(string file)
+        {
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), "../../../Datasets",file);
+        }
+    }
+}
