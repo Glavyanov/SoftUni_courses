d9fb45e0e99aadb24d1e9b9bf75ccc8a5d9ef864
Glavyanov <dimitarglavianov@gmail.com>
2022-07-20 20:18:20 +0300

    AdvancedQuerying-Exercise-Problem11

---
+++ DB_C#/02.Entity_Framework_Core/08.AdvancedQuerying-Exercise/11.CountBooks/BookShop/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            Console.WriteLine(CountBooks(db, int.Parse(Console.ReadLine())));
+
+        }
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            return context.Books
+                .Where(b => b.Title.Length > lengthCheck)
+                .Count();
+        }
+
+    }
+}
