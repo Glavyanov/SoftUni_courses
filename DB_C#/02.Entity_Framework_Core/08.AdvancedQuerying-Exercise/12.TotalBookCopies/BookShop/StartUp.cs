981b50f7f03d4d9b15969343f1784cdbb1d66fa2
Glavyanov <dimitarglavianov@gmail.com>
2022-07-20 20:22:30 +0300

    AdvancedQuerying-Exercise-Problem12

---
+++ DB_C#/02.Entity_Framework_Core/08.AdvancedQuerying-Exercise/12.TotalBookCopies/BookShop/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            Console.WriteLine(CountCopiesByAuthor(db));
+
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            var bookCopies = context.Authors
+                .Include(a => a.Books)
+                .Select(a => new
+                {
+                    a.FirstName,
+                    a.LastName,
+                    Copies = a.Books.Sum(b => b.Copies)
+                })
+                .OrderByDescending(a => a.Copies)
+                .ToArray();
+            var sb = new StringBuilder();
+            foreach (var b in bookCopies)
+            {
+                sb.AppendLine($"{b.FirstName} {b.LastName} - {b.Copies}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
