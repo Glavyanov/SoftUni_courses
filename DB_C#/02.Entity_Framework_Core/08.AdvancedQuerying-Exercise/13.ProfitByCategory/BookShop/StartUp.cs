f359fce97b0d3eb29535bc066b388ba5067e1ca5
Glavyanov <dimitarglavianov@gmail.com>
2022-07-20 20:24:24 +0300

    AdvancedQuerying-Exercise-Problem13

---
+++ DB_C#/02.Entity_Framework_Core/08.AdvancedQuerying-Exercise/13.ProfitByCategory/BookShop/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            Console.WriteLine(GetTotalProfitByCategory(db));
+
+        }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            var categories = context.Categories
+                .Select(c => new
+                {
+                    c.Name,
+                    Total = c.CategoryBooks.Sum(c => c.Book.Copies * c.Book.Price)
+                })
+                .OrderByDescending(c => c.Total)
+                .ThenBy(c => c.Name)
+                .ToArray();
+            StringBuilder sb = new StringBuilder();
+            foreach (var c in categories)
+            {
+                sb.AppendLine($"{c.Name} ${c.Total:F2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
