8a4580658e9ec4c29d164e3c1e9902cf68e88c4c
Glavyanov <dimitarglavianov@gmail.com>
2022-07-22 22:27:07 +0300

    AdvancedQuerying-Exercise-Problem16

---
+++ DB_C#/02.Entity_Framework_Core/08.AdvancedQuerying-Exercise/16.RemoveBooks/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            Console.WriteLine(RemoveBooks(db));
+
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.Copies < 4200);
+            int result = books.Count();
+            context.Books.RemoveRange(books);
+            context.SaveChanges();
+
+            return result;
+        }
+
+    }
+}
