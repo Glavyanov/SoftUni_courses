c26d2773e0b74c3c8679b7c6b818463982a582b6
Glavyanov <dimitarglavianov@gmail.com>
2022-07-22 22:24:47 +0300

    AdvancedQuerying-Exercise-Problem15

---
+++ DB_C#/02.Entity_Framework_Core/08.AdvancedQuerying-Exercise/15.IncreasePrices/BookShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            Console.WriteLine(IncreasePrices(db));
+
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate.Value.Year < 2010);
+            foreach (var b in books)
+            {
+                b.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+
+    }
+}
