573396b9b44ea9a8ca893423bc24d8914652eec6
Glavyanov <dimitarglavianov@gmail.com>
2022-08-03 19:32:46 +0300

    XMLProcessing-Exercise-Problem 01

---
+++ DB_C#/02.Entity_Framework_Core/11.XMLProcessing-Exercise/01.ImportUsers/Models/CategoryProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductShop.Models
+{
+    public class CategoryProduct
+    {
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ DB_C#/02.Entity_Framework_Core/11.XMLProcessing-Exercise/01.ImportUsers/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using ProductShop.Data;
+using ProductShop.Dtos.Categories;
+using ProductShop.Dtos.CategoryProducts;
+using ProductShop.Dtos.Products;
+using ProductShop.Dtos.Users;
+using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        private static string filePath;
+
+        private static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            DatasetsFilePath("users.xml");
+            string xml = File.ReadAllText(filePath);
+            Console.WriteLine(ImportUsers(context, xml));
+
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            XmlRootAttribute root = new XmlRootAttribute("Users");
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportUserDto[]), root);
+
+            ImportUserDto[] userDtos;
+            using (StringReader reader = new StringReader(inputXml))
+            {
+                userDtos = ((ImportUserDto[])serializer.Deserialize(reader)).Where(IsValid).ToArray();
+            }
+
+            InitializeMapper();
+            User[] users = mapper.Map<User[]>(userDtos);
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+        private static void DatasetsFilePath(string file)
+        {
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), "../../../Datasets", file);
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResult, true);
+            return isValid;
+        }
+
+        private static void InitializeMapper()
+        {
+            mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+        }
+
+    }
+}
