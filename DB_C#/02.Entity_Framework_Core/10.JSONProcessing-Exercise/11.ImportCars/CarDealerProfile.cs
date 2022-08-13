372dad19d043dff778570bb0d7655151fa4c4a86
Glavyanov <dimitarglavianov@gmail.com>
2022-08-13 21:07:55 +0300

    JSONProcessing-Exercise-Problem 11

---
+++ DB_C#/02.Entity_Framework_Core/10.JSONProcessing-Exercise/11.ImportCars/CarDealerProfile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using CarDealer.DTO.Cars;
+using CarDealer.DTO.Customer;
+using CarDealer.DTO.Parts;
+using CarDealer.DTO.Sales;
+using CarDealer.DTO.Suppliers;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Suppliers
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Parts
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Customers
+            this.CreateMap<ImportCustomerDto, Customer>();
+            this.CreateMap<Customer, ExportCustomerDto>()
+                .ForMember(dest => dest.BirthDate, mo => mo.MapFrom(src => src.BirthDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)));
+
+            //Sales
+            this.CreateMap<ImportSalesDto, Sale>();
+        }
+    }
+}
---
+++ DB_C#/02.Entity_Framework_Core/10.JSONProcessing-Exercise/11.ImportCars/StartUp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CarDealer.Data;
+using CarDealer.DTO.Cars;
+using CarDealer.DTO.Customer;
+using CarDealer.DTO.Parts;
+using CarDealer.DTO.Sales;
+using CarDealer.DTO.Suppliers;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        private static string filePath;
+
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+            Mapper.Initialize(cfg => cfg.AddProfile(typeof(CarDealerProfile)));
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            DatasetsFilePath("cars.json");
+            string json = File.ReadAllText(filePath);
+            Console.WriteLine(ImportCars(context, json));
+
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            ImportCarDto[] carDtos = JsonConvert.DeserializeObject<ImportCarDto[]>(inputJson)
+                .Where(IsValid)
+                .ToArray();
+
+            List<Car> cars = new List<Car>();
+            foreach (var carDto in carDtos)
+            {
+                Car car = new Car
+                {
+                    Make = carDto.Make,
+                    Model = carDto.Model,
+                    TravelledDistance = carDto.TravelledDistance,
+
+                };
+                foreach (var part in carDto.PartsId.Distinct())
+                {
+                    var partcar = new PartCar
+                    {
+                        CarId = car.Id,
+                        PartId = part
+                    };
+                    car.PartCars.Add(partcar);
+                }
+                cars.Add(car);
+            }
+
+            context.Cars.AddRange(cars);
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Count}.";
+        }
+
+        private static void DatasetsFilePath(string file)
+        {
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), "../../../Datasets", file);
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResult, true);
+            return isValid;
+        }
+    }
+}
