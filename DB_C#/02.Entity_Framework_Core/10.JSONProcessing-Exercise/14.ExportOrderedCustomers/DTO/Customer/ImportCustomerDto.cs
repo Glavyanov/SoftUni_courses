84d74dbf90d9800bfea17862815f6f4e089a9473
Glavyanov <dimitarglavianov@gmail.com>
2022-08-16 23:07:37 +0300

    JSONProcessing-Exercise-Problem 14

---
+++ DB_C#/02.Entity_Framework_Core/10.JSONProcessing-Exercise/14.ExportOrderedCustomers/DTO/Customer/ImportCustomerDto.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace CarDealer.DTO.Customer
+{
+    [JsonObject]
+    public class ImportCustomerDto
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("birthDate")]
+        public DateTime BirthDate { get; set; }
+
+        [JsonProperty("isYoungDriver")]
+        public bool IsYoungDriver { get; set; }
+    }
+}
---
+++ DB_C#/02.Entity_Framework_Core/10.JSONProcessing-Exercise/14.ExportOrderedCustomers/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Newtonsoft.Json;
+
+using CarDealer.Data;
+using CarDealer.Models;
+using CarDealer.DTO.Cars;
+using CarDealer.DTO.Parts;
+using CarDealer.DTO.Sales;
+using CarDealer.DTO.Customer;
+using CarDealer.DTO.Suppliers;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        private static string filePath;
+
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+            Mapper.Initialize(cfg => cfg.AddProfile(typeof(CarDealerProfile)));
+
+            ResultsFilePath("ordered-customers.json");
+            File.WriteAllText(filePath, GetOrderedCustomers(context));
+
+        }
+
+        public static string GetOrderedCustomers(CarDealerContext context)
+        {
+            ExportCustomerDto[] customers = context.Customers
+                .OrderBy(c => c.BirthDate)
+                .ThenBy(c => c.IsYoungDriver)
+                .ProjectTo<ExportCustomerDto>()
+                .ToArray();
+            return JsonConvert.SerializeObject(customers, Formatting.Indented);
+        }
+
+        private static void ResultsFilePath(string file)
+        {
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), "../../../Results", file);
+        }
+
+    }
+}
