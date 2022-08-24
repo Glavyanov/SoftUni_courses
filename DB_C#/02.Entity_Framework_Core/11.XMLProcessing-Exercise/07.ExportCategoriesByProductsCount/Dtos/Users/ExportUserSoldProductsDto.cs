f7fa4154c86754efebe146c920ffceba6b1b18c3
Glavyanov <dimitarglavianov@gmail.com>
2022-08-24 21:42:52 +0300

    XMLProcessing-Exercise-Problem 07

---
+++ DB_C#/02.Entity_Framework_Core/11.XMLProcessing-Exercise/07.ExportCategoriesByProductsCount/Dtos/Users/ExportUserSoldProductsDto.cs
@@ -0,0 +1,19 @@
+﻿using ProductShop.Dtos.Products;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Users
+{
+    [XmlType("User")]
+    public class ExportUserSoldProductsDto
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlArray("soldProducts")]
+        public ExportProductSoldProductsDto[] SoldProducts { get; set; }
+
+    }
+}
---
+++ DB_C#/02.Entity_Framework_Core/11.XMLProcessing-Exercise/07.ExportCategoriesByProductsCount/ProductShopProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using ProductShop.Dtos.Categories;
+using ProductShop.Dtos.CategoryProducts;
+using ProductShop.Dtos.Products;
+using ProductShop.Dtos.Users;
+using ProductShop.Models;
+using System.Linq;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //Users
+            this.CreateMap<ImportUserDto, User>();
+            this.CreateMap<User, ExportUserSoldProductsDto>()
+                .ForMember(dest => dest.SoldProducts, mo => mo.MapFrom(src => src.ProductsSold));
+
+
+            //Products
+            this.CreateMap<ImportProductDto, Product>();
+            this.CreateMap<Product, ExportProductDto>()
+                .ForMember(dest => dest.BuyerFullName, mo => mo.MapFrom(src => src.BuyerId.HasValue ?$"{src.Buyer.FirstName} {src.Buyer.LastName}" : null));
+            this.CreateMap<Product, ExportProductSoldProductsDto>();
+
+            //Categories
+            this.CreateMap<ImportCategoryDto, Category>();
+
+            //CategoryProducts
+            this.CreateMap<ImportCategoryProductsDto, CategoryProduct>();
+            this.CreateMap<Category, ExportCategoryDto>()
+                .ForMember(dest => dest.Count, mo => mo.MapFrom(src => src.CategoryProducts.Count))
+                .ForMember(dest => dest.AveragePrice, mo => mo.MapFrom(src => src.CategoryProducts.Average(p => p.Product.Price)))
+                .ForMember(dest => dest.TotalRevenue, mo =>
+                    mo.MapFrom(src => src.CategoryProducts.Sum(e => e.Product.Price)));
+        }
+    }
+}
