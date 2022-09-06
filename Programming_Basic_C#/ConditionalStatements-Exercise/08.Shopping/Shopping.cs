d3441a375e647adff8790228bc049f306d07f686
Glavyanov <dimitarglavianov@gmail.com>
2022-09-06 16:23:31 +0300

    Hold Conditional Statements - Exercise C#

---
+++ Programming_Basic_C#/ConditionalStatements-Exercise/08.Shopping/Shopping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08Shopping
+{
+    internal class Shopping
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int countVideoCards = int.Parse(Console.ReadLine()), countCPU = int.Parse(Console.ReadLine()), countRAM = int.Parse(Console.ReadLine());
+
+            double priceVideoCards = countVideoCards * 250, priceCPU = priceVideoCards * 0.35 * countCPU, priceRAM = priceVideoCards * 0.1 * countRAM, total = priceCPU + priceRAM + priceVideoCards;
+            
+             _= countVideoCards > countCPU ? total *= 0.85 : total;
+
+            Console.WriteLine(budget >= total ? $"You have {(budget - total):F2} leva left!" : $"Not enough money! You need {(total - budget):F2} leva more!");
+
+        }
+    }
+}
