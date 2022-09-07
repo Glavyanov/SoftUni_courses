13404804a0fcf1c3cf1b5b13c9b9c962b35f8baa
Glavyanov <dimitarglavianov@gmail.com>
2022-09-07 20:08:35 +0300

    XMLProcessing-Exercise-Problem 16

---
+++ DB_C#/02.Entity_Framework_Core/11.XMLProcessing-Exercise/16.ExportLocalSuppliers/CarDealerDtos/Imports/ImportPartDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.CarDealerDtos.Imports
+{
+    [XmlType("Part")]
+    public class ImportPartDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+        [XmlElement("quantity")]
+        public int Quantity { get; set; }
+
+
+        [XmlElement("supplierId")]
+        public int SupplierId { get; set; }
+
+    }
+}
---
+++ DB_C#/02.Entity_Framework_Core/11.XMLProcessing-Exercise/16.ExportLocalSuppliers/StartUp.cs
@@ -0,0 +1,77 @@
+﻿namespace CarDealer
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Microsoft.EntityFrameworkCore;
+
+    using CarDealer.Data;
+    using CarDealer.Models;
+    using CarDealer.CarDealerDtos.Imports;
+    using CarDealer.CarDealerDtos.Exports;
+    
+
+    public class StartUp
+    {
+        private static IMapper mapper;
+
+        private static string filePath;
+
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+
+            ResultsFilePath("local-suppliers.xml");
+            File.WriteAllText(filePath, GetLocalSuppliers(context));
+
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            InitializeMapper();
+            ExportSupplierDto[] supplierDtos = context.Suppliers
+                .AsNoTracking()
+                .Where(s => !s.IsImporter)
+                .ProjectTo<ExportSupplierDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return Serialize(supplierDtos, "suppliers");
+        }
+
+        private static void InitializeMapper()
+        {
+            mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+        }
+
+        private static void ResultsFilePath(string file)
+        {
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), "../../../Results", file);
+        }
+
+        private static string Serialize<T>(T[] dtoT, string root)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(root);
+            XmlSerializer serializer = new XmlSerializer(typeof(T[]), xmlRootAttribute);
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            StringBuilder sb = new StringBuilder();
+            using (StringWriter writer = new StringWriter(sb))
+            {
+                serializer.Serialize(writer, dtoT, namespaces);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
