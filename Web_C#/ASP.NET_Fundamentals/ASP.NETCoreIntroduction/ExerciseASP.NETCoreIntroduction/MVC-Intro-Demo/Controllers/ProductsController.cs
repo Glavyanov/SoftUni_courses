f4babae855d49c54841f7fec44939fc69cdeba9d
Glavyanov <dimitarglavianov@gmail.com>
2022-09-25 21:45:09 +0300

    Add method in ProductsController

--- Web_C#/ASP.NET_Fundamentals/ASP.NETCoreIntroduction/ExerciseASP.NETCoreIntroduction/MVC-Intro-Demo/Controllers/ProductsController.cs
+++ Web_C#/ASP.NET_Fundamentals/ASP.NETCoreIntroduction/ExerciseASP.NETCoreIntroduction/MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -6,6 +6,8 @@
     using MVC_Intro_Demo.Models;
 
     using Microsoft.AspNetCore.Mvc;
+    using System.Net.Mime;
+    using System.Net.Http.Headers;
 
     public class ProductsController : Controller
     {
@@ -64,6 +66,21 @@
             }
 
             return Content(sb.ToString());
+        }
+
+        [HttpGet]
+        public IActionResult AllAsTextFile()
+        {
+            StringBuilder productsAsText = new();
+            foreach (var pr in this.products)
+            {
+                productsAsText.AppendLine($"Product {pr.Id}: {pr.Name} - {pr.Price}lv");
+            }
+
+            Response.Headers.Add("Content-Disposition", "attachment;");
+            byte[] textArr = Encoding.ASCII.GetBytes(productsAsText.ToString());
+            
+            return File(textArr, contentType: "text/plain");
         }
 
     }
