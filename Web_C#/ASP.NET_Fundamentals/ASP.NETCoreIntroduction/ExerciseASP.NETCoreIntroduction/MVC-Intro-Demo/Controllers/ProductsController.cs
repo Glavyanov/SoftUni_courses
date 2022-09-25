2b9c2a97a47d7fcc92dcfedac382c64933476995
Glavyanov <dimitarglavianov@gmail.com>
2022-09-25 17:46:20 +0300

    Add ProductViewModel, ProductsController and View for all products

---
+++ Web_C#/ASP.NET_Fundamentals/ASP.NETCoreIntroduction/ExerciseASP.NETCoreIntroduction/MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IEnumerable<ProductViewModel> products = 
+            new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00M
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50M
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50M
+            }
+        };
+
+        [HttpGet]
+        public IActionResult All()
+        {
+            return View(this.products);
+        }
+
+    }
+}
---
+++ Web_C#/ASP.NET_Fundamentals/ASP.NETCoreIntroduction/ExerciseASP.NETCoreIntroduction/MVC-Intro-Demo/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Intro_Demo.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+    }
+}
