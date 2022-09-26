25bd1ec9853ec80d7496e08f2d2cb00728d84cb1
Glavyanov <dimitarglavianov@gmail.com>
2022-09-27 00:49:45 +0300

    Adding on ChatApp ASP.NET Core Web App (Model-View-Controller)

---
+++ Web_C#/ASP.NET_Fundamentals/ASP.NETCoreIntroduction/ExerciseASP.NETCoreIntroduction/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,39 @@
+﻿namespace ChatApp.Controllers
+{
+    using ChatApp.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> Messages = new List<KeyValuePair<string, string>>();
+
+        [HttpGet]
+        public IActionResult Show()
+        {
+            if (Messages.Count < 1)
+            {
+                return View(new ChatViewModel());
+            }
+            ChatViewModel chatModel = new()
+            {
+                Messages = Messages.Select(m => new MessageViewModel
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                })
+                .ToList()
+            };
+
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            MessageViewModel newMessage = chat.CurrentMessage;
+            Messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.MessageText));
+
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ Web_C#/ASP.NET_Fundamentals/ASP.NETCoreIntroduction/ExerciseASP.NETCoreIntroduction/ChatApp/Models/ChatViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.Models
+{
+    public class ChatViewModel
+    {
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+
+        public List<MessageViewModel> Messages { get; set; } = null!;
+    }
+}
