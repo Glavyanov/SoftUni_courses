60b3d5557d9a3ccbd0fab62117c41b72f356e977
Glavyanov <dimitarglavianov@gmail.com>
2022-10-01 19:59:33 +0300

    reorder

---
+++ Web_C#/ASP.NET_Fundamentals/02.ASP.NETDatabases/ShoppingList/ShoppingList/Models/ProductNoteDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.Models
+{
+    public class ProductNoteDto
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(3000)]
+        public string? Content { get; set; }
+    }
+}
