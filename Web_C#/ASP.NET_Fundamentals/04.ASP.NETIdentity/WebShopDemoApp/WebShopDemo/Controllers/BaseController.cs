3073d1182bfd9416fb887919e96c89134babe3bd
Glavyanov <dimitarglavianov@gmail.com>
2022-10-13 01:36:12 +0300

    update BaseController and _LoginPartial

--- Web_C#/ASP.NET_Fundamentals/04.ASP.NETIdentity/WebShopDemoApp/WebShopDemo/Controllers/BaseController.cs
+++ Web_C#/ASP.NET_Fundamentals/04.ASP.NETIdentity/WebShopDemoApp/WebShopDemo/Controllers/BaseController.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using static WebShopDemo.Core.Data.ValidationConstants.ClaimsConstants;
+
 
 namespace WebShopDemo.Controllers
 {
     [Authorize]
     public class BaseController : Controller
     {
+        public string UserFirstName 
+        { 
+            get
+            {
+                string firstName = string.Empty;
 
+                if (User != null && User.HasClaim(c => c.Type == FirstName))
+                {
+                    firstName = User.Claims.FirstOrDefault(c => c.Type == FirstName)?.Value ?? firstName;
+                }
+
+                return firstName;
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            ViewBag.UserFirstName = UserFirstName; 
+            base.OnActionExecuted(context);
+        }
     }
 }
