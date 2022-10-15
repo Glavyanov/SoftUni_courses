4ddfb47ebfe1cb3eb6d3a8f823cb882271623cb6
Glavyanov <dimitarglavianov@gmail.com>
2022-10-15 21:35:02 +0300

    Add BoardsController and update _LoginPartial and View Home/Index

---
+++ Web_C#/ASP.NET_Fundamentals/05.WorkshopTaskBoardApp/TaskBoardApp/TaskBoardApp/Controllers/BoardsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskBoardApp.Data;
+
+namespace TaskBoardApp.Controllers
+{
+    public class BoardsController : Controller
+    {
+        private readonly TaskBoardAppDbContext data;
+
+        public BoardsController(TaskBoardAppDbContext _data)
+        {
+            data = _data;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
