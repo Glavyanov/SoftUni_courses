997d28779e254fd33227bc3298eb87d9e72d51fc
Glavyanov <dimitarglavianov@gmail.com>
2022-10-15 21:56:34 +0300

    Add Action Index in BoardsController

--- Web_C#/ASP.NET_Fundamentals/05.WorkshopTaskBoardApp/TaskBoardApp/TaskBoardApp/Controllers/BoardsController.cs
+++ Web_C#/ASP.NET_Fundamentals/05.WorkshopTaskBoardApp/TaskBoardApp/TaskBoardApp/Controllers/BoardsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TaskBoardApp.Data;
+using TaskBoardApp.Models;
+using TaskBoardApp.Models.Tasks;
 
 namespace TaskBoardApp.Controllers
 {
@@ -12,9 +15,24 @@ namespace TaskBoardApp.Controllers
             data = _data;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var boards = await this.data.Boards
+                .AsNoTracking()
+                .Select(b => new BoardViewModel()
+                {
+                    Id = b.Id,
+                    Name = b.Name,
+                    Tasks = b.Tasks.Select(t => new TaskViewModel()
+                    {
+                        Id = t.Id,
+                        Title = t.Title,
+                        Description = t.Description,
+                        Owner = t.Owner.UserName
+                    }).ToList()
+                }).ToListAsync();
+
+            return View(boards);
         }
     }
 }
