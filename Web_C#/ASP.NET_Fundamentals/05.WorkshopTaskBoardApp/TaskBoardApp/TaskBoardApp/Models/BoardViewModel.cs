6575e39fd14c4a58e8d719434c6c25876641083f
Glavyanov <dimitarglavianov@gmail.com>
2022-10-15 21:42:55 +0300

    Add BoardViewModel and TaskViewModel

---
+++ Web_C#/ASP.NET_Fundamentals/05.WorkshopTaskBoardApp/TaskBoardApp/TaskBoardApp/Models/BoardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using TaskBoardApp.Models.Tasks;
+
+namespace TaskBoardApp.Models
+{
+    public class BoardViewModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; } = null!;
+
+        public IEnumerable<TaskViewModel> Tasks { get; set; } = new List<TaskViewModel>();
+    }
+}
---
+++ Web_C#/ASP.NET_Fundamentals/05.WorkshopTaskBoardApp/TaskBoardApp/TaskBoardApp/Models/Tasks/TaskViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskBoardApp.Models.Tasks
+{
+    public class TaskViewModel
+    {
+        public int Id { get; init; }
+
+        public string Title { get; init; } = null!;
+
+        public string Description { get; init; } = null!;
+
+        public string Owner { get; init; } = null!;
+    }
+}
