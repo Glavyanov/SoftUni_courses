3e04f5ac7a9841e25c921eeba85c0b9cc27c3d01
Glavyanov <dimitarglavianov@gmail.com>
2022-10-20 22:00:14 +0300

    update LoginViewModel

--- Web_C#/ASP.NET_Fundamentals/06.ExamPrep/Watchlist/Models/LoginViewModel.cs
+++ Web_C#/ASP.NET_Fundamentals/06.ExamPrep/Watchlist/Models/LoginViewModel.cs
@@ -1,8 +1,14 @@
-﻿namespace Watchlist.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Watchlist.Models
 {
     public class LoginViewModel
     {
+        [Required]
         public string UserName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
     }
 }
