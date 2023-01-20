6c4267d35512612f4b914ef9f9f74d9bf94a7278
Glavyanov <dimitarglavianov@gmail.com>
2023-01-20 16:00:05 +0200

    Recursion and Backtracking 06 and 07 tasks

---
+++ Algorithms_C#/AlgorithmsFundamentals/01.RecursionAndBacktracking/06.8QueensPuzzle/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._8QueensPuzzle
+{
+    public class Program
+    {
+        private static HashSet<int> attackedRows = new HashSet<int>();
+        private static HashSet<int> attackedCols = new HashSet<int>();
+        private static HashSet<int> attackedLeftDiag = new HashSet<int>();
+        private static HashSet<int> attackedRightDiag = new HashSet<int>();
+
+        static void Main(string[] args)
+        {
+            bool[,] board = new bool[8, 8];
+
+            PutQueen(board, 0);
+        }
+
+        private static void PutQueen(bool[,] board, int row)
+        {
+            if (row >= board.GetLength(0))
+            {
+                PrintBoard(board);
+                return;
+            }
+
+            for (int col = 0; col < board.GetLength(1); col++)
+            {
+
+                if (!Occupated(row,col))
+                {
+
+                    board[row, col] = true;
+                    attackedRows.Add(row);
+                    attackedCols.Add(col);
+                    attackedLeftDiag.Add(row - col);
+                    attackedRightDiag.Add(row + col);
+
+                    PutQueen(board, row + 1);
+
+                    board[row, col] = false;
+                    attackedRows.Remove(row);
+                    attackedCols.Remove(col);
+                    attackedLeftDiag.Remove(row - col);
+                    attackedRightDiag.Remove(row + col);
+                }
+            }
+        }
+
+        private static bool Occupated(int row, int col)
+        {
+            return attackedRows.Contains(row) ||
+                attackedCols.Contains(col) ||
+                attackedLeftDiag.Contains(row - col) ||
+                attackedRightDiag.Contains(row + col);
+        }
+
+        private static void PrintBoard(bool[,] board)
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    if (board[row, col])
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Algorithms_C#/AlgorithmsFundamentals/01.RecursionAndBacktracking/07.RecursiveFibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07.RecursiveFibonacci
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFibonacci(n));
+        }
+
+        private static int GetFibonacci(int n)
+        {
+            if (n is 0)
+            {
+                return 1;
+            }
+
+            if (n is 1)
+            {
+                return 1;
+            }
+
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}
