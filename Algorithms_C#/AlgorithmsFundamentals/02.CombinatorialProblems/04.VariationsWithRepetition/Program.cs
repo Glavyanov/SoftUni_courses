9e058b589a076df662955c20e763d525091bda3f
Glavyanov <dimitarglavianov@gmail.com>
2023-01-30 00:52:25 +0200

    Add Combinatorial Problems 04 Task

---
+++ Algorithms_C#/AlgorithmsFundamentals/02.CombinatorialProblems/04.VariationsWithRepetition/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04.VariationsWithRepetition
+{
+    public class Program
+    {
+        private static string[] input;
+
+        private static string[] output;
+
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            output = new string[int.Parse(Console.ReadLine())];
+
+            VariationWithRepetition(0);
+        }
+        private static void VariationWithRepetition(int index)
+        {
+            if (index >= output.Length)
+            {
+                Console.WriteLine(string.Join(' ', output));
+                return;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[index] = input[i];
+                VariationWithRepetition(index + 1);
+            }
+        }
+    }
+}
