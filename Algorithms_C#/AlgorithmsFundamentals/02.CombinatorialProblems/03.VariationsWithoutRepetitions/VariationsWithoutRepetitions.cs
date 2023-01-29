4155f984096cf8088ef37a7a351bdc3ec3a594df
Glavyanov <dimitarglavianov@gmail.com>
2023-01-29 23:16:35 +0200

    Add Combinatorial Problems 03 Task

---
+++ Algorithms_C#/AlgorithmsFundamentals/02.CombinatorialProblems/03.VariationsWithoutRepetitions/VariationsWithoutRepetitions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _03.VariationsWithoutRepetitions
+{
+    public class VariationsWithoutRepetitions
+    {
+        private static string[] input;
+
+        private static string[] output;
+
+        private static int num;
+
+        private static bool[] used;
+
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            num = int.Parse(Console.ReadLine());
+
+            output = new string[num];
+
+            used = new bool[input.Length];
+
+            VariationWORepetition(0);
+        }
+
+        private static void VariationWORepetition(int index)
+        {
+            if (index >= num)
+            {
+                Console.WriteLine(string.Join(' ', output));
+                return;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    output[index] = input[i];
+                    VariationWORepetition(index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
