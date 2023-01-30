2ad1c2614334228fbf4b59be4fd67fbd1c126598
Glavyanov <dimitarglavianov@gmail.com>
2023-01-30 21:45:07 +0200

    Add Combinatorial Problems 05, 06 and 07 task

---
+++ Algorithms_C#/AlgorithmsFundamentals/02.CombinatorialProblems/06.CombinationsWithRepetition/CombinationsWithRepetition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05.CombinationsWithoutRepetition
+{
+    public class CombinationsWithRepetition
+    {
+        private static string[] elements;
+
+        private static string[] result;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int k = int.Parse(Console.ReadLine());
+            result = new string[k];
+
+            CombinationsWORepeat(0, 0);
+        }
+
+        private static void CombinationsWORepeat(int index, int start)
+        {
+            if (index >= result.Length)
+            {
+                Console.WriteLine(string.Join(' ', result));
+                return;
+            }
+
+            for (int i = start; i < elements.Length; i++)
+            {
+                result[index] = elements[i];
+                CombinationsWORepeat(index + 1, i);
+            }
+        }
+    }
+}
---
+++ Algorithms_C#/AlgorithmsFundamentals/02.CombinatorialProblems/07.NChooseKCount/NChooseKCount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07.NChooseKCount
+{
+    public class NChooseKCount
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBinom(n, k));
+        }
+
+        private static int GetBinom(int row, int col)
+        {
+            if (row <= 1 || col <= 0 || col == row)
+            {
+                return 1;
+            }
+
+            return GetBinom(row - 1, col) + GetBinom(row - 1, col - 1);
+        }
+    }
+}
