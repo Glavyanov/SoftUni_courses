4e052069e86aadf2496c1fe6c88d76975f1fcc08
Glavyanov <dimitarglavianov@gmail.com>
2023-02-02 11:56:03 +0200

    Add Recursion and Combinatorial Problems 02 Task

---
+++ Algorithms_C#/AlgorithmsFundamentals/03.RecursionAndCombinatorialProblems/02.NestedLoopsToRecursion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02.NestedLoopsToRecursion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Loop(new int[n]);
+        }
+
+        private static void Loop(int[] arr, int index = 0)
+        {
+            if (index >= arr.Length)
+            {
+                Console.WriteLine(string.Join(' ', arr));
+                return;
+            }
+
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                arr[index] = i;
+                Loop(arr, index + 1);
+            }
+        }
+    }
+}
