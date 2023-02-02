bf366beec9e1d760bc3f1103fd088e59bc085552
Glavyanov <dimitarglavianov@gmail.com>
2023-02-02 11:03:50 +0200

    Add Recursion and Combinatorial Problems 01 Task

---
+++ Algorithms_C#/AlgorithmsFundamentals/03.RecursionAndCombinatorialProblems/01.ReverseArray/ReverseArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.ReverseArray
+{
+    public class ReverseArray
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Reverse(arr, 0, arr.Length - 1);
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        private static void Reverse(string[] arr, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            (arr[start], arr[end]) = (arr[end], arr[start]);
+            Reverse(arr, ++start, --end);
+        }
+    }
+}
